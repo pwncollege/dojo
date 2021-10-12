00fb258334cbed1580728d60a2abda7965e9a22d
Connor Nelson <Connor@ConnorNelson.com>
2021-10-12 12:55:23 -0700

    Create 1024 loopback devices for home mounts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,6 +30,15 @@ if [ ! -f /opt/pwn.college/data/homes/homefs ]; then
     rm -rf /opt/pwn.college/data/homes/homefs_mount
 fi
 
+for i in $(seq 1 1024); do
+    if [ -e /dev/loop$i ]; then
+        continue
+    fi
+    mknod /dev/loop$i b 7 $i
+    chown --reference=/dev/loop0 /dev/loop$i
+    chmod --reference=/dev/loop0 /dev/loop$i
+done
+
 mkdir -p /opt/pwn.college/data/logging
 
 echo core > /proc/sys/kernel/core_pattern
