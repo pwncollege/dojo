9a1d120b3aa9c8556fd2619e757e403497707026
Connor Nelson <Connor@ConnorNelson.com>
2021-11-12 18:14:00 -0700

    MAC address doesn't matter

--- challenge/kernel/launch.sh
+++ challenge/kernel/launch.sh
@@ -6,7 +6,7 @@
   -device virtio-9p-pci,fsdev=rootfs,mount_tag=/dev/root \
   -fsdev local,id=homefs,path=/home/hacker,security_model=passthrough \
   -device virtio-9p-pci,fsdev=homefs,mount_tag=/home/hacker \
-  -device e1000,mac=00:11:22:33:44:55,netdev=net0 \
+  -device e1000,netdev=net0 \
   -netdev user,id=net0,hostfwd=tcp::22-:22 \
   -nographic \
   -monitor none \
