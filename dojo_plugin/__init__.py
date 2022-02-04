6f10ba0acb15b3047550b146efd0fd81b43a76d9
Connor Nelson <Connor@ConnorNelson.com>
2022-02-04 13:58:30 -0700

    Move DojoChallenge and DojoFlag into __init__

--- dojo_plugin/__init__.py
+++ dojo_plugin/__init__.py
@@ -1,15 +1,17 @@
 import sys
 import os
 
+from itsdangerous.exc import BadSignature
 from CTFd.models import db
+from CTFd.utils.user import get_current_user
 from CTFd.utils.plugins import override_template
 from CTFd.plugins import register_plugin_assets_directory, register_admin_plugin_menu_bar
-from CTFd.plugins.challenges import CHALLENGE_CLASSES
-from CTFd.plugins.flags import FLAG_CLASSES
+from CTFd.plugins.challenges import CHALLENGE_CLASSES, BaseChallenge
+from CTFd.plugins.flags import FLAG_CLASSES, BaseFlag, FlagException
 
 from .config import bootstrap
-from .api.v1.challenge import DojoChallenge
-from .flag import DojoFlag
+from .models import DojoChallenges
+from .utils import unserialize_user_flag
 from .pages.challenges import challenges_listing, challenges
 from .pages.scoreboard import scoreboard_listing
 from .pages.workspace import workspace
@@ -18,6 +20,34 @@ from .pages.discord import discord
 from .pages.grades import grades
 from .pages.writeups import writeups
 from .api import api
+
+
+class DojoChallenge(BaseChallenge):
+    id = "dojo"
+    name = "dojo"
+    challenge_model = DojoChallenges
+
+
+class DojoFlag(BaseFlag):
+    name = "dojo"
+
+    @staticmethod
+    def compare(chal_key_obj, provided):
+        current_account_id = get_current_user().account_id
+        current_challenge_id = chal_key_obj.challenge_id
+
+        try:
+            account_id, challenge_id = unserialize_user_flag(provided)
+        except BadSignature:
+            return False
+
+        if account_id != current_account_id:
+            raise FlagException("This flag is not yours!")
+
+        if challenge_id != current_challenge_id:
+            raise FlagException("This flag is not for this challenge!")
+
+        return True
 
 
 def load(app):
