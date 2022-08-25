5318b763201dc9a68799c8d08b655c1dcdfeb7ac
Yan <yans@yancomm.net>
2022-08-24 23:39:31 -0700

    Display the module grade (including extra credit) in the module page.

--- dojo_plugin/pages/challenges.py
+++ dojo_plugin/pages/challenges.py
@@ -1,9 +1,12 @@
 import datetime
+import pytz
 
 from flask import Blueprint, render_template, abort
 from CTFd.models import db, Solves, Challenges
 from CTFd.utils.user import get_current_user
 from CTFd.utils.decorators.visibility import check_challenge_visibility
+
+from sqlalchemy import String, DateTime
 
 from ..utils import get_current_challenge_id, dojo_route
 
@@ -16,8 +19,12 @@ def solved_challenges(dojo, module_id=None):
     user_id = user.id if user else None
     solves = db.func.count(Solves.id).label("solves")
     solved = db.func.max(Solves.user_id == user_id).label("solved")
+    solve_date = db.func.substr(
+        db.func.max((Solves.user_id == user_id).cast(String)+Solves.date.cast(String)),
+        2, 1000
+    ).cast(DateTime).label("solve_date")
     challenges = (
-        db.session.query(Challenges.id, Challenges.name, Challenges.category, solves, solved)
+        db.session.query(Challenges.id, Challenges.name, Challenges.category, solves, solve_date, solved)
         .filter(Challenges.state == "visible", dojo.challenges_query(module_id))
         .outerjoin(Solves, Solves.challenge_id == Challenges.id)
         .group_by(Challenges.id)
@@ -63,11 +70,20 @@ def view_module(dojo, module):
     challenges = solved_challenges(dojo, module_id)
     current_challenge_id = get_current_challenge_id()
 
+    num_timely_solves = len([ c for c in challenges if c.solved and pytz.UTC.localize(c.solve_date) < due ]) if due else 0
+    num_late_solves = len([ c for c in challenges if c.solved and pytz.UTC.localize(c.solve_date) >= due ]) if due else 0
+    num_full_ec_solves = len([ c for c in challenges if c.solved and pytz.UTC.localize(c.solve_date) < ec_full ]) if ec_full else 0
+    num_part_ec_solves = len([ c for c in challenges if c.solved and pytz.UTC.localize(c.solve_date) < ec_part ]) if ec_part else 0
+
     return render_template(
         "module.html",
         dojo=dojo,
         module=module,
         ec_part=ec_part, ec_full=ec_full,
         challenges=challenges,
+        num_timely_solves=num_timely_solves,
+        num_late_solves=num_late_solves,
+        num_full_ec_solves=num_full_ec_solves,
+        num_part_ec_solves=num_part_ec_solves,
         current_challenge_id=current_challenge_id
     )
