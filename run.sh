2ef88b519886346ff2a21151d4d0338720b0bc15
Connor Nelson <Connor@ConnorNelson.com>
2022-08-26 15:19:56 -0700

    Enable development environment

--- run.sh
+++ run.sh
@@ -12,10 +12,13 @@ docker run \
        --rm \
        --volume $DIR/data/docker:/var/lib/docker \
        --volume $DIR/data:/opt/pwn.college/data \
+       --volume $DIR/dojo_plugin:/opt/pwn.college/CTFd/CTFd/plugins/dojo_plugin:ro \
+       --volume $DIR/dojo_theme:/opt/pwn.college/CTFd/CTFd/themes/dojo_theme:ro \
        --publish ${SSH_PORT:-22}:22 \
        --publish ${HTTP_PORT:-80}:80 \
        --publish ${HTTPS_PORT:-443}:443 \
        --env SETUP_HOSTNAME="$SETUP_HOSTNAME" \
+       --env DEV_ENVIRONMENT="$DEV_ENVIRONMENT" \
        --hostname dojo \
        --name pwn.college \
        pwn.college
