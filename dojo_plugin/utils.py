1aba6223bb257fca2e5013bbb1eb1d1766e7aebe
Fish <fishw@asu.edu>
2022-09-07 03:40:32 -0700

    Do not raise mod-by-zero exceptions if no optional files exist for a challenge.

--- dojo_plugin/utils.py
+++ dojo_plugin/utils.py
@@ -106,9 +106,10 @@ def challenge_paths(user, challenge, *, secret=None):
         if not (option.name.startswith(".") or option.name.startswith("_"))
     )
 
-    option_hash = hashlib.sha256(f"{secret}_{user.id}_{challenge.id}".encode()).digest()
-    option = options[int.from_bytes(option_hash[:8], "little") % len(options)]
-    yield from option.iterdir()
+    if options:
+        option_hash = hashlib.sha256(f"{secret}_{user.id}_{challenge.id}".encode()).digest()
+        option = options[int.from_bytes(option_hash[:8], "little") % len(options)]
+        yield from option.iterdir()
 
 
 def simple_tar(path, name=None):
