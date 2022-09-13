0371844e1e1251bfac66c1feff3c7f3ccae23c57
Yan <yans@yancomm.net>
2022-09-12 21:35:47 -0700

    map the dojo plugin and dojo theme into the ctfd container

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ docker run \
        --rm \
        --volume $DIR/data/docker:/var/lib/docker \
        --volume $DIR/data:/opt/pwn.college/data \
+       --volume $DIR/dojo_plugin:/opt/pwn.college/CTFd/CTFd/plugins/dojo_plugin:ro \
+       --volume $DIR/dojo_theme:/opt/pwn.college/CTFd/CTFd/themes/dojo_theme:ro \
        --publish ${SSH_PORT:-22}:22 \
        --publish ${HTTP_PORT:-80}:80 \
        --publish ${HTTPS_PORT:-443}:443 \
