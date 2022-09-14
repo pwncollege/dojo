bdb94074a27237080ea80b094c9bfd02bab97deb
Connor Nelson <Connor@ConnorNelson.com>
2022-09-14 19:36:49 +0000

    Make config values more configurable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 
-ls -al /opt/pwn.college/data
+. /opt/pwn.college/data/config.env
 
 if [ ! "$(ls -A /opt/pwn.college/data/dojos /opt/pwn.college/data/challenges)" ]; then
     echo "Warning: initializing dojo for the first time and no data included, auto populating with data_example"
     cp -r /opt/pwn.college/data_example/* /opt/pwn.college/data
 fi
-
-if [ ! -f /opt/pwn.college/data/config.env ]; then
-    if [ -z "$SETUP_HOSTNAME" ]; then
-        echo "Warning: initializing dojo for the first time and no SETUP_HOSTNAME specified, defaulting to localhost.pwn.college"
-        SETUP_HOSTNAME="localhost.pwn.college"
-    fi
-    cat <<EOF >> /opt/pwn.college/data/config.env
-HOST=$SETUP_HOSTNAME
-SECRET_KEY=$(openssl rand -hex 16)
-DOCKER_PSLR=$(openssl rand -hex 16)
-DISCORD_CLIENT_ID=
-DISCORD_CLIENT_SECRET=
-DISCORD_BOT_TOKEN=
-DISCORD_GUILD_ID=
-EOF
-    echo "Dojo configuration is stored in data/config.env"
-fi
-. /opt/pwn.college/data/config.env
 
 if [ ! -f /opt/pwn.college/data/homes/homefs ]; then
     mkdir -p /opt/pwn.college/data/homes
@@ -48,14 +30,11 @@ for i in $(seq 1 1024); do
     chmod --reference=/dev/loop0 /dev/loop$i
 done
 
-if [ ! -d /opt/pwn.college/data/dms ]; then
-    mkdir -p /opt/pwn.college/data/dms
-    mkdir -p /opt/pwn.college/data/dms/mail-data
-    mkdir -p /opt/pwn.college/data/dms/mail-state
-    mkdir -p /opt/pwn.college/data/dms/mail-logs
-    mkdir -p /opt/pwn.college/data/dms/config
-    echo "hacker@${HOST}|{SHA512-CRYPT}$(openssl passwd -6 hacker)" > /opt/pwn.college/data/dms/config/postfix-accounts.cf
-fi
+mkdir -p /opt/pwn.college/data/dms/config \
+         /opt/pwn.college/data/dms/mail-data \
+         /opt/pwn.college/data/dms/mail-state \
+         /opt/pwn.college/data/dms/mail-logs \
+echo "hacker@${DOJO_HOST}|{SHA512-CRYPT}$(openssl passwd -6 hacker)" > /opt/pwn.college/data/dms/config/postfix-accounts.cf
 
 mkdir -p /opt/pwn.college/data/logging
 
---
+++ dojo.sh
@@ -0,0 +1,79 @@
+#!/bin/sh -e
+
+DIR="$(readlink -f $(dirname $_))"
+
+mkdir -p "$DIR"/data
+touch "$DIR"/data/config.env
+
+if [ "$1" = "init" ]; then
+    > "$DIR"/data/.config.env
+
+    define () {
+        name="$1"
+        default="$2"
+        re="^${name}=\K.*"
+        current="$(env | grep -oP ${re})"
+        defined="$(grep -oP ${re} $DIR/data/config.env)"
+        value="${current:-${defined:-$default}}"
+        echo "${name}=${value}" >> $DIR/data/.config.env
+    }
+
+    define DOJO_HOST localhost.pwn.college
+    define DOJO_SSH_PORT 22
+    define DOJO_HTTP_PORT 80
+    define DOJO_HTTPS_PORT 443
+
+    define SECRET_KEY $(openssl rand -hex 16)
+    define DOCKER_PSLR $(openssl rand -hex 16)
+
+    define DISCORD_CLIENT_ID
+    define DISCORD_CLIENT_SECRET
+    define DISCORD_BOT_TOKEN
+    define DISCORD_GUILD_ID
+
+    mv "$DIR"/data/.config.env "$DIR"/data/config.env
+
+    cat "$DIR"/data/config.env
+fi
+
+. "$DIR"/data/config.env
+
+if [ "$1" = "build" ]; then
+    docker build -t "$DOJO_HOST" .
+
+elif [ "$1" = "run" ]; then
+    $0 build
+    $0 stop
+
+    docker run \
+           --privileged \
+           --detach \
+           --rm \
+           --volume "$DIR"/data/docker:/var/lib/docker \
+           --volume "$DIR"/data:/opt/pwn.college/data \
+           --volume "$DIR"/dojo_plugin:/opt/CTFd/CTFd/plugins/dojo_plugin:ro \
+           --volume "$DIR"/dojo_theme:/opt/CTFd/CTFd/themes/dojo_theme:ro \
+           --publish "$DOJO_SSH_PORT":22 \
+           --publish "$DOJO_HTTP_PORT":80 \
+           --publish "$DOJO_HTTPS_PORT":443 \
+           --hostname "$DOJO_HOST" \
+           --name "$DOJO_HOST" \
+           "$DOJO_HOST"
+
+    docker exec "$DOJO_HOST" logs
+
+elif [ "$1" = "stop" ]; then
+    while [ $(docker ps -q -f name="$DOJO_HOST") ]; do
+        docker kill "$DOJO_HOST" || sleep 1
+    done
+
+elif [ "$1" = "logs" ]; then
+    docker exec -it "$DOJO_HOST" docker logs -f "${2-ctfd}"
+
+elif [ "$1" = "update" ]; then
+    git -C "$DIR" pull
+    git -C "$DIR"/data/dojos pull
+
+    docker exec -it "$DOJO_HOST" docker stop ctfd
+    docker exec -it "$DOJO_HOST" docker start ctfd
+fi
