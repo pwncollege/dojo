c2bed5152076c19128e09967f1bd2c584af531bb
Yan <yans@yancomm.net>
2022-12-03 23:31:44 -0700

    logging during dojo import

--- dojo_plugin/config.py
+++ dojo_plugin/config.py
@@ -1,14 +1,14 @@
 import sqlalchemy
 import warnings
-import yaml
+import logging
 import os
-import re
 
 from CTFd.models import db, Admins, Pages, Flags, Challenges
 from CTFd.cache import cache
 from CTFd.utils import config, set_config
 from .utils import multiprocess_lock
 
+_LOG = logging.getLogger(__name__)
 
 VIRTUAL_HOST = os.getenv("VIRTUAL_HOST")
 HOST_DATA_PATH = os.getenv("HOST_DATA_PATH")
@@ -31,14 +31,19 @@ for config_option in missing_warnings:
     if not config_value:
         warnings.warn(f"Configuration Warning: {config_option} is not set in the environment")
 
-def load_global_dojo(dojo_id, dojo_spec):
+def load_dojo(dojo_id, dojo_spec, user="global"):
     from .models import DojoChallenges, Dojos
+
+    dojo_log = logging.getLogger(f"dojos.{user}.{dojo_id}")
+    dojo_log.info("Initiating dojo load.")
 
     dojo = Dojos.query.filter_by(id=dojo_id).first()
     if not dojo:
         dojo = Dojos(id=dojo_id)
+        dojo_log.info("Dojo is new, adding.")
         db.session.add(dojo)
     elif dojo.data == dojo_spec:
+        dojo_log.info("Dojo is unchanged, aborting update.")
         return
 
     dojo.data = dojo_spec
@@ -47,37 +52,48 @@ def load_global_dojo(dojo_id, dojo_spec):
     assert dojo.config.get("dojo_spec", None) == "v2"
 
     # delete all challenges owned by this dojo
+    dojo_log.info("Deleting existing challenge mappings.")
     deleter = sqlalchemy.delete(DojoChallenges).where(DojoChallenges.dojo_id == dojo.id).execution_options(synchronize_session="fetch")
     db.session.execute(deleter)
     db.session.commit()
 
     # re-load the dojo challenges
     for module_idx,module in enumerate(dojo.config["modules"], start=1):
-        if "challenges" not in module:
+        if "name" not in module:
+            dojo_log.info("Module index %d is missing 'name' field; skipping.", module_idx)
             continue
 
-        for level_idx,challenge_spec in enumerate(module["challenges"], start=1):
-            if "dojo" in challenge_spec:
-                # don't create challenges that are imported from other dojos
-                continue
+        if "challenges" not in module:
+            dojo_log.info("Module %s has no challenges defined. Skipping challenge load.", module["name"])
+            continue
 
+        dojo_log.info("Loading challenges for module %s.", module["name"])
+
+        for level_idx,challenge_spec in enumerate(module["challenges"], start=1):
+            dojo_log.info("Loading module %s challenge %d", module["name"], level_idx)
             description = challenge_spec.get("description", None)
 
             # spec dependent
-            name = None
             category = None
             provider_dojo_id = None
             provider_module = None
 
             if "import" not in challenge_spec:
+                if "name" not in challenge_spec:
+                    dojo_log.info("... challenge is missing a name. Skipping.")
+                    continue
+
                 # if this is our dojo's challenge, make sure it's in the DB
                 name = challenge_spec["name"]
                 category = challenge_spec.get("category", f"{dojo.id}-challenge")
+
+                dojo_log.info("... challenge name: %s", name)
 
                 challenge = Challenges.query.filter_by(
                     name=name, category=category
                 ).first()
                 if not challenge:
+                    dojo_log.info("... challenge is new; creating")
                     challenge = Challenges(
                         name=name,
                         category=category,
@@ -91,24 +107,33 @@ def load_global_dojo(dojo_id, dojo_spec):
                     db.session.add(flag)
                     db.session.commit()
                 elif description is not None and challenge.description != description:
+                    dojo_log.info("... challenge already exists; updating description")
                     challenge.description = description
                     db.session.commit()
             else:
+                if challenge_spec["import"].count("/") != 2:
+                    dojo_log.info("... malformed import statement, should be dojo_id/module_name/challenge_name. Skipping.")
+                    continue
+
                 # if we're importing this from another dojo, do that
-                provider_dojo_id, provider_module, name = challenge_spec["import"].split("/")
+                provider_dojo_id, provider_module, provider_challenge = challenge_spec["import"].split("/")
+                dojo_log.info("... importing from dojo %s, module %s, challenge %s", provider_dojo_id, provider_module, name)
+
                 provider_dojo_challenge = (
                     DojoChallenges.query
                     .join(Challenges, Challenges.id == DojoChallenges.challenge_id)
                     .filter(
                         DojoChallenges.dojo_id==provider_dojo_id, DojoChallenges.module==provider_module,
-                        Challenges.name==name
+                        Challenges.name==provider_challenge
                     )
                 ).first()
                 if not provider_dojo_challenge:
+                    dojo_log.info("... can't find provider challenge; skipping")
                     continue
 
                 challenge = provider_dojo_challenge.challenge
 
+            dojo_log.info("... creating dojo-challenge link for challenge %d", level_idx)
             # then create the DojoChallenge link
             dojo_challenge = DojoChallenges(
                 challenge_id=challenge.id,
@@ -178,6 +203,7 @@ def bootstrap():
         set_config("setup", True)
 
     for dojo_config_path in DOJOS_DIR.glob("*.yml"):
+        _LOG.info("Loading dojo specification %s", dojo_config_path)
         dojo_id = dojo_config_path.stem
         spec = dojo_config_path.read_text()
-        load_global_dojo(dojo_id, spec)
+        load_dojo(dojo_id, spec)
