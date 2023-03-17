b7c47c72b487045648d9912bc732dccff0988b93
Connor Nelson <Connor@ConnorNelson.com>
2023-03-17 18:17:05 +0000

    Add completion report

--- dojo_plugin/pages/users.py
+++ dojo_plugin/pages/users.py
@@ -1,11 +1,15 @@
+import datetime
 import functools
-from flask import Blueprint, render_template, abort
+import itertools
+
+from flask import Blueprint, Response, render_template, abort
+from sqlalchemy.sql import and_
 from CTFd.utils.user import get_current_user
 from CTFd.utils.decorators import authed_only
 from CTFd.models import db, Users, Challenges, Solves
 from CTFd.cache import cache
 
-from ..models import Dojos
+from ..models import Dojos, DojoModules, DojoChallenges
 from ..utils.dojo import dojo_scoreboard_data
 
 
@@ -43,3 +47,26 @@ def view_other(user_id):
 @authed_only
 def view_self():
     return view_hacker(get_current_user())
+
+@users.route("/hacker/completion-report")
+@authed_only
+def view_completion_report():
+    user = get_current_user()
+    solves = (
+        dojo
+        .solves(user=user, ignore_visibility=True)
+        .join(DojoModules, and_(
+            DojoModules.dojo_id == DojoChallenges.dojo_id,
+            DojoModules.module_index == DojoChallenges.module_index))
+        .order_by(Solves.id)
+        .with_entities(Dojos.id, DojoModules.id, DojoChallenges.id, Solves.date)
+        for dojo in Dojos.viewable(user=user)
+    )
+    result = []
+    for dojo_id, module_id, challenge_id, date in itertools.chain.from_iterable(solves):
+        date = date.replace(tzinfo=datetime.timezone.utc)
+        result.append((dojo_id, module_id, challenge_id, date))
+    result.sort(key=lambda row: row[-1])
+    return Response("".join(f"{dojo_id}/{module_id}/{challenge_id} @ {date}\n"
+                            for dojo_id, module_id, challenge_id, date in result),
+                    mimetype="text")
