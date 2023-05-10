a814b1b57671aa309d52e6006c1e461034413574
Yan Shoshitaishvili <yans@yancomm.net>
2023-05-10 14:48:30 -0700

    add global internet access option (#168)

--- script/container-setup.sh
+++ script/container-setup.sh
@@ -19,6 +19,7 @@ define DOJO_ENV development
 define DOJO_CHALLENGE challenge-minimal
 define SECRET_KEY $(openssl rand -hex 16)
 define DOCKER_PSLR $(openssl rand -hex 16)
+define INTERNET_FOR_ALL False
 define MAIL_SERVER
 define MAIL_PORT
 define MAIL_USERNAME
