8fced1a2bd63b46f1cb8629d851fe880e799c405
Connor Nelson <Connor@ConnorNelson.com>
2023-08-09 13:33:49 -0700

    Redirect workspace referers

--- dojo_plugin/pages/workspace.py
+++ dojo_plugin/pages/workspace.py
@@ -1,4 +1,6 @@
-from flask import request, Blueprint, render_template
+from urllib.parse import urlparse
+
+from flask import request, Blueprint, render_template, redirect, url_for
 from CTFd.utils.user import get_current_user
 from CTFd.utils.decorators import authed_only
 
@@ -24,3 +26,13 @@ def forward_workspace(path=""):
     assert request.full_path.startswith(prefix)
     path = request.full_path[len(prefix):]
     return redirect_user_socket(get_current_user(), ".local/share/code-server/workspace.socket", f"/{path}")
+
+
+@workspace.before_request
+def redirect_workspace_referers():
+    referer = request.headers.get("Referer", "")
+    referer_path = urlparse(referer).path
+    current_path = request.path
+
+    if referer_path.startswith("/workspace/") and not current_path.startswith("/workspace/"):
+        return redirect(url_for("pwncollege_workspace.forward_workspace", path=current_path))
