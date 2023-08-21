aae4af35236c7bcca9dbd575384229f23b54f115
Connor Nelson <Connor@ConnorNelson.com>
2023-08-21 11:42:38 -0700

    Fix NoneType error

--- dojo_plugin/pages/course.py
+++ dojo_plugin/pages/course.py
@@ -193,9 +193,9 @@ def grade(dojo, users_query):
             previous_user_id = user_id
         if module_id is not None:
             module_solves[module_id] = (
-                int(checkpoint_solves),
-                int(due_solves),
-                int(all_solves),
+                int(checkpoint_solves) if checkpoint_solves is not None else 0,
+                int(due_solves) if checkpoint_solves is not None else 0,
+                int(all_solves) if checkpoint_solves is not None else 0,
             )
     if user_id:
         yield result(user_id)
