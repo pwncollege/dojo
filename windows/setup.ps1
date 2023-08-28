36e365453130682f3132232e423a975864ddaec1
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-08-27 19:19:05 -0700

    set default ssh shell to powershell

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -30,6 +30,8 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 # Autostart sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+# set default shell to powershell
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
 # Confirm the Firewall rule is configured. It should be created automatically by setup.
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
