836ec829872220cf99305f350434e339c48047b1
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-08-29 21:23:35 -0700

    copy flag to C: and make user admin if practice enabled
    TODO: fix c:\flag permissions ACL

--- windows/startup.ps1
+++ windows/startup.ps1
@@ -2,3 +2,7 @@
 & "C:\Program Files (x86)\WinFsp\bin\fsreg.bat" virtiofs "D:\virtio-win\viofs\2k22\amd64\virtiofs.exe" "-t %1 -m %2"
 & "C:\Program Files (x86)\WinFsp\bin\launchctl-x64.exe" start virtiofs viofsY challenge Y:
 & "C:\Program Files (x86)\WinFsp\bin\launchctl-x64.exe" start virtiofs viofsZ home Z:
+type Y:\flag > C:\flag
+if (Test-Path Y:\practice-mode-enabled) {
+  Add-LocalGroupMember -Group "Administrators" -Member hacker
+}
