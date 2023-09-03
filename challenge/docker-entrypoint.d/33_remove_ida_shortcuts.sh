eaa0bbb697048deef919cb678f6a48c958e2192d
Yan <yans@yancomm.net>
2023-09-03 00:29:14 -0700

    make startup scripts more resilient to missing xfce files

--- challenge/docker-entrypoint.d/33_remove_ida_shortcuts.sh
+++ challenge/docker-entrypoint.d/33_remove_ida_shortcuts.sh
@@ -4,5 +4,5 @@
 if [ ! -d /opt/ida ]
 then
     rm -rf /home/hacker/.config/xfce4/panel/launcher-13
-    sed -i -e '/<property name="plugin-13" type="string" value="launcher">/,/<\/property>/d' /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+    [ ! -f /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ] || sed -i -e '/<property name="plugin-13" type="string" value="launcher">/,/<\/property>/d' /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 fi
