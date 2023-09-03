da75c586cb47aa5b645d4f9ff43dede65987f857
Yan <yans@yancomm.net>
2023-09-03 00:31:11 -0700

    log the output of the startup script for easier future debugging

--- challenge/docker-entrypoint.sh
+++ challenge/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+exec >/tmp/.startup_log 2>&1
+chmod 600 /tmp/.startup_log
+
 for SCRIPT in /opt/pwn.college/docker-entrypoint.d/*
 do
 	echo "[*] docker-entrypoint running script: $SCRIPT"
