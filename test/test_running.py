4262b2a8fb0cc56456d634c0ef8dd13ae1e00355
Yan <yans@yancomm.net>
2023-09-04 00:55:22 -0700

    capitalize global constant

--- test/test_running.py
+++ test/test_running.py
@@ -1,9 +1,9 @@
 import pytest
 import requests
 
-unauthenticated_urls = ["/", "/dojos", "/login", "/register"]
+UNAUTHENTICATED_URLS = ["/", "/dojos", "/login", "/register"]
 
-@pytest.mark.parametrize("endpoint", unauthenticated_urls)
+@pytest.mark.parametrize("endpoint", UNAUTHENTICATED_URLS)
 def test_unauthenticated_return_200(endpoint):
     response = requests.get(f"http://localhost{endpoint}")
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
