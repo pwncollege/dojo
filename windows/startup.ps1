0f119e206158c6b4c28b13da46580fd8a6d8a24a
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-09-09 23:33:55 -0700

    fix flag permissions

--- windows/startup.ps1
+++ windows/startup.ps1
@@ -5,19 +5,27 @@ Remove-LocalGroupMember -Group "Administrators" -Member hacker
 & "C:\Program Files (x86)\WinFsp\bin\launchctl-x64.exe" start virtiofs viofsZ home Z:
 
 $null > C:\flag
+
+# crash course in the footguns of NTFS's ACL based permissions system that I learned
+#  the hard way:
+# - a "Deny" rule will always take precedence over an "Allow" rule.
+#   For example: Admins Allow Read + Users Deny Read 
+#   This will result in no one being able to read the flag because they all fall under
+#    the "Users" rule.
+# - ACLs inherit from the parent directory by default unless explicitly disabled.
+#   Combined with the previous rule, this means that if an "Allow" rule is inherited
+#    from the parent directory, there is no way to counteract it with a "Deny" rule.
+#   You have to disable inheritance instead.
+
 $flagAcl = New-Object System.Security.AccessControl.FileSecurity
-$adminsRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+# important: do not inherit from the parent directory.
+$flagAcl.SetAccessRuleProtection($true, $false)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule(
   "Administrators",
-  "Read",
+  "FullControl",
   "Allow"
 )
-$flagAcl.AddAccessRule($adminsRule)
-$denyRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
-  "Users",
-  "Read",
-  "Deny"
-)
-$flagAcl.AddAccessRule($denyRule)
+$flagAcl.AddAccessRule($rule)
 Set-Acl -Path C:\flag -AclObject $flagAcl
 Set-Content -Path C:\flag -Value (Get-Content Y:\flag)
 Remove-Item -Path Y:\flag -Force
