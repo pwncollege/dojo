982491d9c6d0aee54692e058d82b9a023220700b
Zeeshan <2018me167@student.uet.edu.pk>
2023-10-05 23:26:21 +0500

    Expose additional wechall data (#217)
    * wechall - validate user
    
    * wechall - get user score
    
    * wechall - check if user exists
    
    * wechall - applied requested changes
    
    * added global rank and total users
    
    * official dojos and distinct solves
    
    * properly get distinct solves
    
    * ouch 5.42s
    
    * no len()

--- dojo_plugin/api/v1/score.py
+++ dojo_plugin/api/v1/score.py
@@ -1,10 +1,10 @@
 from flask import request
 from flask_restx import Namespace, Resource
 from CTFd.cache import cache
-from CTFd.models import Users
+from CTFd.models import Users, db, Solves
 from CTFd.utils.decorators import ratelimit
 
-from ...models import Dojos
+from ...models import Dojos, DojoChallenges
 
 score_namespace = Namespace("score")
 
@@ -23,6 +23,21 @@ class ValidateUser(Resource):
             return {"error": "`username` and `email` parameters are required"}, 400
 
         return int(bool(Users.query.filter_by(name=username, email=email).first()))
+
+def global_scoreboard_data(fields=None):
+    fields = fields or []
+    order_by = (db.func.count().desc(), db.func.max(Solves.id))
+    return (
+        DojoChallenges.solves()
+        .filter(Dojos.official == 1)
+        .group_by(Solves.user_id)
+        .join(Users, Users.id == Solves.user_id)
+        .with_entities(db.func.row_number().over(order_by=order_by).label("rank"),
+            db.func.count(db.distinct(Solves.challenge_id)).label("solves"),
+            Solves.user_id,
+            *fields)
+        .order_by(db.desc("solves"))
+    )
 
 @score_namespace.route("")
 class ScoreUser(Resource):
@@ -44,4 +59,9 @@ class ScoreUser(Resource):
         user_score = sum(1 for solve in user.solves if solve.challenge_id in official_challenge_ids)
         max_score = len(official_challenge_ids)
 
-        return f"{user_score}:{max_score}"
+        user_count = Users.query.count()
+        result = global_scoreboard_data()
+        # if user has not solved anything, show rank as the last user `user_count`
+        rank = next((item.rank for item in result.all() if item.user_id == user.id), user_count)
+        # since chall count is the same as user_score, they can be reused
+        return f"{rank}:{user_score}:{max_score}:{user_score}:{max_score}:{user_count}"
