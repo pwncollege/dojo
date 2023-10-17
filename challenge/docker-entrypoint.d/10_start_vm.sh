c3637061a9f8d7109af24aa6ecdf4b3dd00a9173
Connor Nelson <Connor@ConnorNelson.com>
2023-10-17 12:40:29 -0700

    Fix setuid permission
    The hacker user cannot chmod these files

--- challenge/docker-entrypoint.d/10_start_vm.sh
+++ challenge/docker-entrypoint.d/10_start_vm.sh
@@ -3,13 +3,9 @@
 if ! find /challenge -name '*.ko' -exec false {} +
 then
   vm start
-else
-  chmod -s "$(which vm)"
 fi
 
 if ! find /challenge -name '*.exe' -exec false {} +
 then
   windows start
-else
-  chmod -s "$(which windows)"
 fi
