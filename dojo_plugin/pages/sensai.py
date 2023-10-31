11bc525a8ec9368f4032d73c62835b10db7aa438
Connor Nelson <Connor@ConnorNelson.com>
2023-10-31 11:04:00 -0700

    Fix websocket route

--- dojo_plugin/pages/sensai.py
+++ dojo_plugin/pages/sensai.py
@@ -21,8 +21,8 @@ def view_sensai():
 
 @sensai.route("/sensai/", methods=["GET", "POST"])
 @sensai.route("/sensai/<path:path>", methods=["GET", "POST"])
-@sensai.route("/workspace/", methods=["GET", "POST"], websocket=True)
-@sensai.route("/workspace/<path:path>", methods=["GET", "POST"], websocket=True)
+@sensai.route("/sensai/", websocket=True)
+@sensai.route("/sensai/<path:path>", websocket=True)
 @authed_only
 @bypass_csrf_protection
 def forward_sensai(path=""):
