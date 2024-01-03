952ed8d567890c8eece67abb4246df6f9287cf2a
Adam Doupe <adamdoupe@gmail.com>
2024-01-03 14:11:09 -0800

    First attempt at test of scoreboard and caching

--- test/test_running.py
+++ test/test_running.py
@@ -189,3 +189,66 @@ def test_workspace_practice_challenge(random_user):
         workspace_run("sudo -v", user=user)
     except subprocess.CalledProcessError as e:
         assert False, f"Expected sudo to succeed, but got: {(e.stdout, e.stderr)}"
+
+
+def get_all_standings(session, dojo):
+    """
+    Return a big list of all the standings, going through all the available pages.
+    """
+    to_return = []
+
+    page_number = 1
+    done = False
+
+    while not done:
+        response = session.get(f"{PROTO}://{HOST}/pwncollege_api/v1/scoreboard/{dojo}/_/0/{page_number}")
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+        response = response.json()
+
+        to_return.extend(response["standings"])
+
+        next_page = page_number + 1
+
+        if next_page in response["pages"]:
+            page_number += 1
+        else:
+            done = True
+
+    return to_return
+
+
+@pytest.mark.dependency(depends=["test_workspace_challenge"])
+def test_scoreboard(random_user):
+    user, session = random_user
+
+    dojo = "example"
+
+    prior_standings = get_all_standings(session, dojo)
+
+    # if test_workspace_challenge passed correctly, then we should get a valid flag here
+    result = workspace_run("/challenge/apple", user=user)
+    flag = result.stdout.strip()
+
+    # submit the flag
+    data = {"challenge_id": "blah",
+            "submission": flag}
+    response = session.post(f"{PROTO}://{HOST}/api/v1/challenges/attempt", data=data)
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+    assert response.json()["success"], f"Expected to successfully submit flag"
+
+    # check the scoreboard: is it updated?
+
+    new_standings = get_all_standings(session, dojo)
+    assert len(prior_standings) != len(new_standings), "Expected to have a new entry in the standings"
+
+    found_me = False
+    for standing in new_standings:
+        if standing['name'] == user:
+            found_me = True
+            break
+    assert found_me, f"Unable to find new user {user} in new standings after solving a challenge"
+    
+    
+    
+
+
