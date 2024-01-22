8e8c618dd20949fe557abf427c0cc4e8eee7622f
Yan <yans@yancomm.net>
2024-01-21 22:19:40 -0700

    assert failure when appropriate

--- test/test_running.py
+++ test/test_running.py
@@ -128,6 +128,8 @@ def test_join_dojo(admin_session, random_user):
     sql = f"SELECT id FROM users WHERE name = '{random_user_name}'"
     db_result = dojo_run("db", input=sql)
     random_user_id = int(db_result.stdout.split()[1])
+    response = admin_session.post(f"{PROTO}://{HOST}/pwncollege_api/v1/dojo/example/promote-admin", data={})
+    assert response.status_code == 400
     response = admin_session.post(f"{PROTO}://{HOST}/pwncollege_api/v1/dojo/example/promote-admin", data={"user_id": random_user_id})
     assert response.status_code == 200
     response = admin_session.get(f"{PROTO}://{HOST}/dojo/example/admin/")
