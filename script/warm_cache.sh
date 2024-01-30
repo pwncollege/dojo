d4bf05be75f709e1b4e359ca9d949edfbae7953c
Yan <yans@yancomm.net>
2024-01-29 21:53:07 -0700

    change warm_cache.sh to not use the interactive shell

--- script/warm_cache.sh
+++ script/warm_cache.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-dojo flask <<END
-from ..utils import scores
-ds = scores.dojo_scores()
-ms = scores.module_scores()
-print(f"""
-	  {len(ds)=}
-	  {len(ms)=}
-""")
-END
+dojo flask /opt/CTFd/CTFd/plugins/dojo_plugin/scripts/warm_cache.py
