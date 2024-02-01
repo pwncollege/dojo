fad5f3a81911a3c9de388cbf31dadbabede3f7a9
Yan <yans@yancomm.net>
2024-02-01 01:22:30 -0700

    cleanup

--- dojo_plugin/utils/dojo.py
+++ dojo_plugin/utils/dojo.py
@@ -355,7 +355,7 @@ def dojo_git_command(dojo, *args):
 
 
 def dojo_update(dojo):
-    dojo_git_command(dojo, "pull", "--recurse-submodules")
+    dojo_git_command(dojo, "pull")
     dojo_git_command(dojo, "submodule", "init")
     dojo_git_command(dojo, "submodule", "update")
     return dojo_from_dir(dojo.path, dojo=dojo)
