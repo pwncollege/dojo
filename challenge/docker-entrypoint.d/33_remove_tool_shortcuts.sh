8684e978545fa0af7e877fe295a33a9aaed272ae
Yan <yans@yancomm.net>
2024-02-07 21:59:09 +0000

    generic script to remove icons for apps that are not installed

---
+++ challenge/docker-entrypoint.d/33_remove_tool_shortcuts.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+for LAUNCHER in /home/hacker/.config/xfce4/panel/launcher-*
+do
+        NUMBER=${LAUNCHER##*-}
+        EXE=$(cat "$LAUNCHER"/*.desktop | grep Exec | sed -e "s/.*=//")
+        if [ ! -f "$EXE" ]
+        then
+                rm -rf "$LAUNCHER"
+                sed -i -e '/<property name="plugin-$NUMBER" type="string" value="launcher">/,/<\/property>/d' /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+        fi
+done
