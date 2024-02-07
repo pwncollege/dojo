1984fd528ac72fd85fa3d33e823d31b31158ab55
Yan <yans@yancomm.net>
2024-02-07 21:59:09 +0000

    generic script to remove icons for apps that are not installed

--- challenge/docker-entrypoint.d/33_remove_ida_shortcuts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# delete shortcuts to IDA if IDA is not installed
-if [ ! -d /opt/ida ]
-then
-    rm -rf /home/hacker/.config/xfce4/panel/launcher-13
-    [ ! -f /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ] || sed -i -e '/<property name="plugin-13" type="string" value="launcher">/,/<\/property>/d' /home/hacker/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-fi
