25659495f5eee84a37fa3fb621a911b9f16ed4d8
Yan <yans@yancomm.net>
2024-02-10 00:10:31 -0700

    add a test to ensure that belts are cumulative

--- test/test_running.py
+++ test/test_running.py
@@ -270,6 +270,20 @@ def test_belts(belt_dojos, random_user):
         us = next(u for u in scoreboard["standings"] if u["name"] == user_name)
         assert color in us["belt"]
 
+@pytest.mark.dependency(depends=["test_belts"])
+def test_cumulative_belts(belt_dojos, random_user):
+    user_name, session = random_user
+    for color,dojo in reversed(belt_dojos.items()):
+        start_and_solve(user_name, session, dojo, "test", "test")
+        scoreboard = session.get(f"{PROTO}://{HOST}/pwncollege_api/v1/scoreboard/{dojo}/_/0/1").json()
+        us = next(u for u in scoreboard["standings"] if u["name"] == user_name)
+        if color == "orange":
+            # orange is last, so we should get all belts including blue
+            assert "blue" in us["belt"]
+        else:
+            # until orange, we should be stuck in white
+            assert "white" in us["belt"]
+
 @pytest.mark.dependency(depends=["test_start_challenge"])
 @pytest.mark.parametrize("path", ["/flag", "/challenge/apple"])
 def test_workspace_path_exists(path):
