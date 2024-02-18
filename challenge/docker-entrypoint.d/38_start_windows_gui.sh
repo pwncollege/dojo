5e54929b3acf17c628e2590cf212e0b9c2f395a5
Robert Wasinger <robwaz@robwaz.net>
2024-02-18 04:59:50 -0700

    move windows VM into challenge image build
    And you though challenge build times were long before..

--- challenge/docker-entrypoint.d/38_start_windows_gui.sh
+++ challenge/docker-entrypoint.d/38_start_windows_gui.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-if ! find /challenge -name '*.exe' -exec false {} +
-then
-  mkdir -p /tmp/.dojo/vnc /home/hacker/.vnc
-  start-stop-daemon --start \
-                    --pidfile /tmp/.dojo/vnc/websockify-windows.pid \
-                    --make-pidfile \
-                    --background \
-                    --no-close \
-                    --startas /usr/bin/websockify \
-                    -- \
-                    --web /usr/share/novnc/ \
-                    dojo-user:6082 \
-                    localhost:5912 \
-                    </dev/null \
-                    >>/tmp/.dojo/vnc/websockify-windows.log \
-                    2>&1
-fi
+mkdir -p /tmp/.dojo/vnc /home/hacker/.vnc
+start-stop-daemon --start \
+                  --pidfile /tmp/.dojo/vnc/websockify-windows.pid \
+                  --make-pidfile \
+                  --background \
+                  --no-close \
+                  --startas /usr/bin/websockify \
+                  -- \
+                  --web /usr/share/novnc/ \
+                  dojo-user:6082 \
+                  localhost:5912 \
+                  </dev/null \
+                  >>/tmp/.dojo/vnc/websockify-windows.log \
+                  2>&1
