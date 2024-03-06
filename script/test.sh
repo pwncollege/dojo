49fa0fb276215802798c5a0d55e967c015b7b5e2
Yan <yans@yancomm.net>
2024-03-06 15:45:44 -0700

    helper script for local testing

---
+++ script/test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -ex
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+function usage {
+	echo "Usage: $0 [-r DB_BACKUP ] [ -c CONTAINER_NAME ] [ -T ]"
+	echo ""
+	echo "	-r	db backup to restore (relative to dojo/data/backups)"
+	echo "	-c	the name of the dojo container (default: dojo-test)"
+	echo "	-D	use a blank data volume (builds everything from scratch)"
+	echo "	-T	don't run tests"
+	exit
+}
+
+VOLUME_ARGS=("-v" "$PWD:/opt/pwn.college:shared")
+ENV_ARGS=( )
+DB_RESTORE=""
+CONTAINER_NAME=dojo-test
+TEST=yes
+while getopts "r:c:he:TD" OPT
+do
+	case $OPT in
+		r) DB_RESTORE="$OPTARG" ;;
+		c) CONTAINER_NAME="$OPTARG" ;;
+		T) TEST=no ;;
+		D)
+			DATA_DIR=$(mktemp -d)
+			VOLUME_ARGS+=("-v" "$DATA_DIR:/opt/pwn.college/data:shared")
+			;;
+		e) ENV_ARGS+=("-e" "$OPTARG") ;;
+		h) usage ;;
+		?)
+			OPTIND=$(($OPTIND-1))
+			break
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+[ "${#VOLUME_ARGS[@]}" -eq 1 ] && VOLUME_ARGS+=(
+	"-v" "/opt/pwn.college/data/dojos"
+	"-v" "/opt/pwn.college/data/mysql"
+)
+
+export CONTAINER_NAME
+docker kill "$CONTAINER_NAME" 2>/dev/null || echo "No $CONTAINER_NAME container to kill."
+docker rm "$CONTAINER_NAME" 2>/dev/null || echo "No $CONTAINER_NAME container to remove."
+while docker ps -a | grep "$CONTAINER_NAME"; do sleep 1; done
+
+# freaking bad unmount
+mount | grep $PWD | while read -a ENTRY
+do
+	sudo umount "${ENTRY[2]}"
+done
+
+docker run --rm --privileged -d "${VOLUME_ARGS[@]}" "${ENV_ARGS[@]}" -p 2222:22 -p 80:80 -p 443:443 --name "$CONTAINER_NAME" dojo || exit 1
+
+# fix the insane routing thing
+read -a GW <<<$(ip route show default)
+if [[ "${GW[2]}" = 10* ]]
+then
+	docker exec "$CONTAINER_NAME" ip route add "${GW[2]}" via 172.17.0.1
+fi
+
+docker exec "$CONTAINER_NAME" dojo wait
+[ -n "$DB_RESTORE" ] && until docker exec "$CONTAINER_NAME" dojo restore $DB_RESTORE; do sleep 1; done
+
+until curl -s localhost.pwn.college | grep pwn; do sleep 1; done
+[ "$TEST" == "yes" ] && pytest test/test_running.py
