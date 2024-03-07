02187e2ce3ea376fe1e0a235f7c2940de35218a6
Yan <yans@yancomm.net>
2024-03-06 22:15:43 -0700

    refactor test_welcome

--- test/test_welcome.py
+++ test/test_welcome.py
@@ -1,103 +1,107 @@
+import contextlib
 import selenium.webdriver
+import pytest
 import time
 import os
 
-#pylint:disable=unused-argument
+#pylint:disable=unused-argument,redefined-outer-name
 
 from utils import PROTO, HOST, workspace_run
 from selenium.webdriver.common.keys import Keys
 
-def load_vscode_terminal(wd):
-	wd.switch_to.new_window("tab")
-	wd.get(f"{PROTO}://{HOST}/workspace/vscode/")
-	time.sleep(1)
-	wd.switch_to.active_element.send_keys(Keys.CONTROL + Keys.SHIFT + "`")
-	time.sleep(1)
+def webdriver():
+    # workaround for ubuntu
+    gd = "/snap/bin/geckodriver"
+    if os.path.exists(gd):
+        ds = selenium.webdriver.FirefoxService(executable_path=gd)
+        wd = selenium.webdriver.Firefox(service=ds)
+    else:
+        wd = selenium.webdriver.Firefox()
+    return wd
 
-def start_chal(button, result):
-	button.click()
-	while "started" not in result.text:
-		time.sleep(0.5)
-	time.sleep(1)
+@pytest.fixture
+def random_user_webdriver(random_user):
+    random_id,random_session = random_user
+    wd = webdriver()
+    wd.get(f"{PROTO}://{HOST}/login")
+    wd.find_element("id", "name").send_keys(random_id)
+    wd.find_element("id", "password").send_keys(random_id)
+    wd.find_element("id", "_submit").click()
+    return random_id,random_session,wd
 
-def test_welcome(random_user, welcome_dojo):
-	gd = "/snap/bin/geckodriver"
-	if os.path.exists(gd):
-		ds = selenium.webdriver.FirefoxService(executable_path=gd)
-		wd = selenium.webdriver.Firefox(service=ds)
-	else:
-		wd = selenium.webdriver.Firefox()
+@contextlib.contextmanager
+def vscode_terminal(wd):
+    module_window = wd.current_window_handle
 
-	wd.get(f"{PROTO}://{HOST}/login")
+    wd.switch_to.new_window("tab")
+    wd.get(f"{PROTO}://{HOST}/workspace/vscode/")
+    time.sleep(1)
+    wd.switch_to.active_element.send_keys(Keys.CONTROL + Keys.SHIFT + "`")
+    time.sleep(1)
 
-	random_id,_ = random_user
+    yield wd.switch_to.active_element
 
-	wd.find_element("id", "name").send_keys(random_id)
-	wd.find_element("id", "password").send_keys(random_id)
-	wd.find_element("id", "_submit").click()
+    wd.close()
+    wd.switch_to.window(module_window)
 
-	wd.get(f"{PROTO}://{HOST}/welcome/welcome")
-	num_challenges = len(wd.find_elements("id", "challenge-start"))
+# Expands the accordion entry of the challenge
+def challenge_expand(wd, idx):
+    wd.refresh()
+    wd.find_element("id", f"challenges-header-button-{idx}").click()
+    time.sleep(0.5)
 
-	module_window = wd.current_window_handle
+def challenge_start(wd, idx, practice=False):
+    challenge_expand(wd, idx)
+    body = wd.find_element("id", f"challenges-body-{idx}")
+    body.find_element("id", "challenge-practice" if practice else "challenge-start").click()
+    while "started" not in body.find_element("id", "result-message").text:
+        time.sleep(0.5)
+    time.sleep(1)
 
-	for n in range(num_challenges):
-		challenge_header = wd.find_element("id", f"challenges-header-button-{n+1}")
-		challenge_name = challenge_header.text.split("\n")[0]
-		challenge_header.click()
-		time.sleep(0.5)
+def challenge_submit(wd, idx, flag):
+    challenge_expand(wd, idx)
+    body = wd.find_element("id", f"challenges-body-{idx}")
+    body.find_element("id", "challenge-input").send_keys(flag)
+    body.find_element("id", "challenge-submit").click()
+    while "Correct" not in body.find_element("id", "result-message").text:
+        time.sleep(0.5)
 
-		challenge_body = wd.find_element("id", f"challenges-body-{n+1}")
-		start = challenge_body.find_element("id", "challenge-start")
-		practice = challenge_body.find_element("id", "challenge-practice")
-		flagbox = challenge_body.find_element("id", "challenge-input")
-		submit = challenge_body.find_element("id", "challenge-submit")
-		result = challenge_body.find_element("id", "result-message")
+# Gets the accordion entry index
+def challenge_idx(wd, name):
+    num_challenges = len(wd.find_elements("id", "challenge-start"))
+    idx = next(n for n in range(num_challenges) if wd.find_element("id", f"challenges-header-button-{n+1}").text.split("\n")[0] == name)
+    return idx+1
 
-		if   challenge_name == "Using the VSCode Workspace":
-			start_chal(start, result)
-			load_vscode_terminal(wd)
-			wd.switch_to.active_element.send_keys("/challenge/solve | tee /tmp/out\n")
-			time.sleep(5)
-			# can't figure out how to get the output, so here we go
-			flag = workspace_run("tail -n1 /tmp/out", user=random_id).stdout.split()[-1]
-			wd.close()
-			wd.switch_to.window(module_window)
+def test_welcome_vscode(random_user_webdriver, welcome_dojo):
+    random_id, _, wd = random_user_webdriver
+    wd.get(f"{PROTO}://{HOST}/welcome/welcome")
+    idx = challenge_idx(wd, "Using the VSCode Workspace")
 
-			flagbox.send_keys(flag)
-			submit.click()
-			while "Correct" not in result.text:
-				time.sleep(0.5)
-		elif challenge_name == "Using the GUI Desktop":
-			pass
-		elif challenge_name == "Pasting into the Desktop":
-			pass
-		elif challenge_name == "The Flag File":
-			pass
-		elif challenge_name == "Using Practice Mode":
-			start_chal(practice, result)
-			load_vscode_terminal(wd)
-			wd.switch_to.active_element.send_keys("sudo chmod 644 /challenge/secret\n")
-			wd.switch_to.active_element.send_keys("cp /challenge/secret /home/hacker/\n")
-			wd.close()
-			wd.switch_to.window(module_window)
+    challenge_start(wd, idx)
+    with vscode_terminal(wd) as vs:
+        vs.send_keys("/challenge/solve | tee /tmp/out\n")
+        time.sleep(5)
 
-			start_chal(start, result)
-			load_vscode_terminal(wd)
-			wd.switch_to.active_element.send_keys("/challenge/solve < secret | tee /tmp/out\n")
-			time.sleep(5)
-			# can't figure out how to get the output, so here we go
-			flag = workspace_run("tail -n1 /tmp/out", user=random_id).stdout.split()[-1]
-			wd.close()
-			wd.switch_to.window(module_window)
+    flag = workspace_run("tail -n1 /tmp/out", user=random_id).stdout.split()[-1]
+    challenge_submit(wd, idx, flag)
+    wd.close()
 
-			flagbox.send_keys(flag)
-			submit.click()
-			while "Correct" not in result.text:
-				time.sleep(0.5)
-		elif challenge_name == "Persistent Home Directories - One":
-			pass
-		elif challenge_name == "Persistent Home Directories - Two":
-			pass
-		else:
-			raise AssertionError(f"Unexpected challenge name: {challenge_header.text}")
+def test_welcome_practice(random_user_webdriver, welcome_dojo):
+    random_id, _, wd = random_user_webdriver
+    wd.get(f"{PROTO}://{HOST}/welcome/welcome")
+    idx = challenge_idx(wd, "Using Practice Mode")
+
+    challenge_start(wd, idx, practice=True)
+    with vscode_terminal(wd) as vs:
+        vs.send_keys("sudo chmod 644 /challenge/secret\n")
+        vs.send_keys("cp /challenge/secret /home/hacker/\n")
+        time.sleep(1)
+
+    challenge_start(wd, idx, practice=False)
+    with vscode_terminal(wd) as vs:
+        vs.send_keys("/challenge/solve < secret | tee /tmp/out\n")
+        time.sleep(5)
+
+    flag = workspace_run("tail -n1 /tmp/out", user=random_id).stdout.split()[-1]
+    challenge_submit(wd, idx, flag)
+    wd.close()
