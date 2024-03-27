b071ec7224ecb0919a2957efb98aae7f45162391
Adam Doupe <adamdoupe@gmail.com>
2024-03-27 00:22:48 -0700

    unholy production commits

--- sshd/auth.py
+++ sshd/auth.py
@@ -2,9 +2,26 @@
 
 import sys
 import pathlib
+import os
+import subprocess
 
-import docker
+# adamd: insanity to reload the environment varaibles from the docker compose
 
+global_env = "/etc/environment"
+if os.path.exists(global_env):
+    with open(global_env, "r") as f:
+        for line in f.readlines():
+            res = line.strip().split("=", maxsplit=1)
+            if res and len(res) == 2:
+                key = res[0]
+                value = res[1]
+                os.environ[key] = value
+
+
+DB_HOST = os.environ.get('DB_HOST', "db")
+DB_NAME = os.environ.get('DB_NAME', "ctfd")
+DB_USER = os.environ.get('DB_USER', "ctfd")
+DB_PASS = os.environ.get('DB_PASS', "ctfd")
 
 def error(msg):
     print(msg, file=sys.stderr)
@@ -13,20 +30,13 @@ def error(msg):
 
 def main():
     enter_path = pathlib.Path(__file__).parent.resolve() / "enter.py"
-    client = docker.from_env()
 
-    try:
-        container = client.containers.get("db")
-    except docker.errors.NotFound:
-        error("Error: ctfd is not running!")
+    connect_arg = f"-h{DB_HOST}" if DB_HOST else ""
+    result = subprocess.run(["mysql", connect_arg, f"-p{DB_PASS}", f"-u{DB_USER}", f"-D{DB_NAME}", "-sNe", 'select value, user_id from ssh_keys;'], stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        error(f"Error: db query exited with code '{result.returncode}'")
 
-    result = container.exec_run(
-        "mysql -pctfd -Dctfd -sNe 'select value, user_id from ssh_keys;'"
-    )
-    if result.exit_code != 0:
-        error(f"Error: db query exited with code '{result.exit_code}'")
-
-    for row in result.output.strip().split(b"\n"):
+    for row in result.stdout.strip().split(b"\n"):
         key, user_id = row.decode().split("\t")
         print(f'command="{enter_path} user_{user_id}" {key}')
 
