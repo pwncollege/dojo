2a62598f06e09ecec2b14ef2e9753959bd5d356c
Adam Doupe <adamdoupe@gmail.com>
2024-03-29 11:10:38 -0700

    Confirm to the insane mariadb dockerfile

--- db/start.sh
+++ db/start.sh
@@ -2,7 +2,7 @@
 
 if [ "${DB_EXTERNAL:-no}" = "no" ]
 then
-    mysqld --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --wait_timeout=28800 --log-warnings=0
+    /docker-entrypoint.sh mysqld --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --wait_timeout=28800 --log-warnings=0
 else
     while true; do sleep 86400; done
 fi
