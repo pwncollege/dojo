20a30686cfe0e0815b697aac165e832cfcc00ba5
Yan <yans@yancomm.net>
2024-04-11 14:24:23 -0700

    double testcase reliability

--- test/test_welcome.py
+++ test/test_welcome.py
@@ -35,9 +35,9 @@ def vscode_terminal(wd):
 
     wd.switch_to.new_window("tab")
     wd.get(f"{PROTO}://{HOST}/workspace/vscode/")
-    time.sleep(1)
+    time.sleep(2)
     wd.switch_to.active_element.send_keys(Keys.CONTROL + Keys.SHIFT + "`")
-    time.sleep(1)
+    time.sleep(2)
 
     yield wd.switch_to.active_element
 
@@ -50,11 +50,11 @@ def desktop_terminal(wd, user_id):
 
     wd.switch_to.new_window("tab")
     wd.get(f"{PROTO}://{HOST}/workspace/desktop")
-    time.sleep(1)
+    time.sleep(2)
     workspace_run("DISPLAY=:42.0 xterm &", user=user_id)
     wd.switch_to.frame("workspace")
     e = wd.find_element("id", "noVNC_keyboardinput")
-    time.sleep(1)
+    time.sleep(2)
 
     yield e
 
