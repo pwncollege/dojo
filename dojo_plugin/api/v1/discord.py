d03baaee3db94bd35dd83c497da5c88815b3193f
Connor Nelson <Connor@ConnorNelson.com>
2024-05-16 10:35:30 -0700

    Discord: Clear cache on discord disconnect

--- dojo_plugin/api/v1/discord.py
+++ dojo_plugin/api/v1/discord.py
@@ -1,8 +1,11 @@
 from flask_restx import Namespace, Resource
+from CTFd.cache import cache
 from CTFd.models import db
+from CTFd.utils.decorators import authed_only
 from CTFd.utils.user import get_current_user
 
 from ...models import DiscordUsers
+from ...utils.discord import get_discord_user
 
 
 discord_namespace = Namespace("discord", description="Endpoint to manage discord")
@@ -10,7 +13,10 @@ discord_namespace = Namespace("discord", description="Endpoint to manage discord
 
 @discord_namespace.route("")
 class Discord(Resource):
+    @authed_only
     def delete(self):
-        DiscordUsers.query.filter_by(user=get_current_user()).delete()
+        user = get_current_user()
+        DiscordUsers.query.filter_by(user=user).delete()
         db.session.commit()
+        cache.delete_memoized(get_discord_user, user.id)
         return {"success": True}
