b81c38033cb03954cd1ccc6759da2b05e9762d5f
Ruth Whendell <spydev@duck.com>
2024-06-06 12:40:45 -0700

    Add overriding ripgrep to code server override

--- challenge/challenge-nix/coder-server-override.nix
+++ challenge/challenge-nix/coder-server-override.nix
@@ -45,7 +45,19 @@ let
           )
       }
 
-      echo HFIFIBFFBI
+    mv "$out"/libexec/code-server/lib/vscode/node_modules/@vscode/ripgrep/bin/rg "$out"/libexec/code-server/lib/vscode/node_modules/@vscode/ripgrep/bin/rg.orig
+    echo "$out"/libexec/code-server/lib/vscode/node_modules/@vscode/ripgrep/bin/rg
+    cp <<EOF "$out"/libexec/code-server/lib/vscode/node_modules/@vscode/ripgrep/bin/rg
+    #!/usr/bin/env python
+    
+    import sys
+    import os
+    
+    sys.argv[0] += ".orig"
+    if "--follow" in sys.argv:
+        sys.argv.remove("--follow")
+    os.execv(sys.argv[0], sys.argv)
+    EOF  
     '';
 
   });
