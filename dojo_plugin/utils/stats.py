2096a9ec1be5e8a149802ebbb0b693ee182f4234
Yan <yans@yancomm.net>
2024-07-17 23:18:59 -0700

    filter out hidden users

--- dojo_plugin/utils/stats.py
+++ dojo_plugin/utils/stats.py
@@ -1,6 +1,7 @@
 import docker
 
 from CTFd.cache import cache
+from CTFd.models import Users
 
 @cache.memoize(timeout=60)
 def container_stats():
@@ -9,8 +10,8 @@ def container_stats():
         #"label": [ f"dojo.dojo_id={dojo.reference_id}", f"dojo.module_id={module.id}" ]
     }, ignore_removed=True)
     return [ {
-            'user': c.name.split("_")[-1],
+            'user': int(c.name.split("_")[-1]),
             'dojo': c.labels['dojo.dojo_id'],
             'module': c.labels['dojo.module_id'],
             'challenge': c.labels['dojo.challenge_id']
-    } for c in user_containers ]
+    } for c in user_containers if not Users.query.where(Users.id==int(c.name.split("_")[-1])).one().hidden ]
