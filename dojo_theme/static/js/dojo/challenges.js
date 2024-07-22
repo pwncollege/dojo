a6b86e47d1c1c4f1432f8f3b2e93fed01a42b441
JensHouses <82268654+JensHouses@users.noreply.github.com>
2024-07-22 10:30:48 +0200

    cleaning up some mess

--- dojo_theme/static/js/dojo/challenges.js
+++ dojo_theme/static/js/dojo/challenges.js
@@ -1,5 +1,3 @@
-
-
 function submitChallenge(event) {
     event.preventDefault();
     const item = $(event.currentTarget).closest(".accordion-item");
@@ -212,14 +210,6 @@ function startChallenge(event) {
         result_message.html("Submission request failed: " + ((error || {}).message || error));
         result_notification.addClass('alert alert-warning alert-dismissable text-center');
     })
-
-console.log(
-    "Start Challenge button clicked",
-    event,
-    challenge,
-    practice
-);
-    event.stopPropagation();
 }
 
 
