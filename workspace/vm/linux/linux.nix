80b3359c4f2118bf0f3f6bcb7a16ba0d27a4c183
Your Name <spy@cosmics.dev>
2024-07-24 15:44:54 +0000

    Add patched config for ssh

--- workspace/vm/linux/linux.nix
+++ workspace/vm/linux/linux.nix
@@ -3,7 +3,8 @@
 let
   kernelPackage = pkgs.linuxKernel.kernels.linux_5_4;
   kernel = pkgs.callPackage ./kernel.nix { inherit pkgs kernelPackage; };
-  initrd = pkgs.callPackage ./initrd.nix { inherit pkgs; };
+  ssh = pkgs.callPackage ./ssh.nix { inherit pkgs; };
+  initrd = pkgs.callPackage ./initrd.nix { inherit pkgs ssh; };
   linux-script = pkgs.callPackage ./linux-script.nix { inherit pkgs service kernel initrd; };
 in
 { 
@@ -11,5 +12,6 @@ in
     linux-script
     kernel
     initrd
+    ssh
   ];
 }
---
+++ workspace/vm/linux/ssh.nix
@@ -0,0 +1,33 @@
+{ pkgs }:
+
+let
+sshdPatch = pkgs.writeText "sshd_patch.diff" ''
+    *** a/etc/ssh/sshd_config
+    --- b/etc/ssh/sshd_config
+    ***************
+    *** 32 ****
+    ! #PermitRootLogin prohibit-password
+    --- 32 ----
+    ! PermitRootLogin yes
+    ***************
+    *** 57,58 ****
+    ! #PasswordAuthentication yes
+    ! #PermitEmptyPasswords no
+    --- 57,58 ----
+    ! PasswordAuthentication yes
+    ! PermitEmptyPasswords yes
+    ***************
+    *** 116 ****
+    --- 117,118 ----
+    + 
+    + UsePrivilegeSeparation no
+  '';
+in
+pkgs.openssh.overrideAttrs (oldAttrs: {
+    # Disable checks which error
+    doCheck = false;
+
+    postInstall = (oldAttrs.postInstall or "") + ''
+      patch -p1 -d $out < ${sshdPatch}
+    '';
+})
