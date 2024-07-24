89dbadaea108ef4da75006067f8cfde5e7acbbcb
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-07-23 17:09:34 -0700

    format with nixpkgs-fmt

--- workspace/flake.nix
+++ workspace/flake.nix
@@ -7,21 +7,22 @@
 
   outputs = { self, nixpkgs }: {
     packages = {
-      x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          config = {
-            allowUnfree = true;
+      x86_64-linux =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+            };
           };
-        };
 
-        init = import ./init.nix { inherit pkgs; };
-        ssh-entrypoint = import ./ssh-entrypoint.nix { inherit pkgs; };
-        service = import ./services/service.nix { inherit pkgs; };
-        code-service = import ./services/code.nix { inherit pkgs; };
-        desktop-service = import ./services/desktop.nix { inherit pkgs; };
+          init = import ./init.nix { inherit pkgs; };
+          ssh-entrypoint = import ./ssh-entrypoint.nix { inherit pkgs; };
+          service = import ./services/service.nix { inherit pkgs; };
+          code-service = import ./services/code.nix { inherit pkgs; };
+          desktop-service = import ./services/desktop.nix { inherit pkgs; };
 
-        ldd-wrapper = (pkgs.writeShellScriptBin "ldd" ''
+          ldd-wrapper = (pkgs.writeShellScriptBin "ldd" ''
             ldd=/usr/bin/ldd
             for arg in "$@"; do
               case "$arg" in
@@ -34,53 +35,54 @@
               esac
             done
             exec "$ldd" "$@"
-        '');
+          '');
 
-        additional = import ./additional/additional.nix { inherit pkgs; };
+          additional = import ./additional/additional.nix { inherit pkgs; };
 
-        corePackages = with pkgs; [
-          bashInteractive
-          cacert
-          coreutils
-          curl
-          findutils
-          glibc
-          glibcLocales
-          gawk
-          gnugrep
-          gnused
-          hostname
-          iproute2
-          (lib.hiPrio ldd-wrapper)
-          less
-          man
-          ncurses
-          procps
-          util-linux
-          wget
-          which
+          corePackages = with pkgs; [
+            bashInteractive
+            cacert
+            coreutils
+            curl
+            findutils
+            glibc
+            glibcLocales
+            gawk
+            gnugrep
+            gnused
+            hostname
+            iproute2
+            (lib.hiPrio ldd-wrapper)
+            less
+            man
+            ncurses
+            procps
+            util-linux
+            wget
+            which
 
-          init
-          ssh-entrypoint
-          service
-          code-service
-          desktop-service
+            init
+            ssh-entrypoint
+            service
+            code-service
+            desktop-service
 
-          gdb
-        ];
+            gdb
+          ];
 
-        fullPackages = corePackages ++ additional.packages;
+          fullPackages = corePackages ++ additional.packages;
 
-        buildDojoEnv = name: paths: pkgs.buildEnv {
-          name = "dojo-workspace-${name}";
-          inherit paths;
+          buildDojoEnv = name: paths: pkgs.buildEnv {
+            name = "dojo-workspace-${name}";
+            inherit paths;
+          };
+
+        in
+        {
+          default = buildDojoEnv "core" corePackages;
+          core = buildDojoEnv "core" corePackages;
+          full = buildDojoEnv "full" fullPackages;
         };
-
-      in {
-        default = buildDojoEnv "core" corePackages;
-        core = buildDojoEnv "core" corePackages;
-        full = buildDojoEnv "full" fullPackages;
-      };
     };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux;
