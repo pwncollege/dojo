e03f1d9271f5c2d932bdffef24d5a24508beb232
Connor Nelson <Connor@ConnorNelson.com>
2024-08-01 18:44:30 -0700

    Workspace: Fix ida-free cloud decompiler (#492)

--- workspace/additional/additional.nix
+++ workspace/additional/additional.nix
@@ -16,6 +16,22 @@ let
     selenium
   ]);
 
+  ida-free = pkgs.ida-free.overrideAttrs (oldAttrs: {
+    # This patch fixes IDA free to make sure libssl is correctly loaded in order to use the decompiler; this should be removed once the issue is fixed upstream.
+    # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/by-name/id/ida-free/package.nix#L116
+    preInstall = ''
+    eval _"$(declare -f wrapProgram)"
+    wrapProgram() {
+      local program="$1"
+      shift
+      _wrapProgram \
+        "$program" \
+        --prefix LD_LIBRARY_PATH : ${pkgs.lib.getLib pkgs.openssl}/lib \
+        "$@"
+    }
+    '';
+  });
+
 in
 {
   packages = with pkgs; [
