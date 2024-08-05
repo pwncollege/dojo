7fdb6c123b228a8cfa00fc47e2ea655b021c7d6e
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-08-04 19:10:39 -0700

    desktop-win revived

--- dojo_plugin/pages/workspace.py
+++ dojo_plugin/pages/workspace.py
@@ -18,15 +18,28 @@ port_names = {
     "challenge": 80,
     "code": 8080,
     "desktop": 6080,
-    "desktop-windows": 6082,
+    "desktop-win": 6082,
 }
-ondemand_services = { "code", "desktop", "desktop-windows" }
+on_demand_services = { "code", "desktop", "desktop-win" }
 
 def container_password(container, *args):
     key = container.labels["dojo.auth_token"].encode()
     message = "-".join(args).encode()
     return hmac.HMAC(key, message, "sha256").hexdigest()
 
+def start_on_demand_service(user, service_name):
+    if service_name not in on_demand_services:
+        return
+    try:
+        exec_run(
+            f"/run/current-system/sw/bin/dojo-{service_name}",
+            workspace_user="hacker",
+            user_id=user.id,
+            assert_success=True,
+        )
+    except docker.errors.NotFound:
+        return False
+    return True
 
 @workspace.route("/workspace/desktop")
 @authed_only
@@ -57,8 +70,8 @@ def view_desktop():
 
     vnc_params = {
         "autoconnect": 1,
-        "reconnect": 0,
-        "reconnect_delay": 1000,
+        "reconnect": 1,
+        "reconnect_delay": 200,
         "resize": "remote",
         "path": url_for("pwncollege_workspace.forward_workspace", service=service, service_path="websockify"),
         "view_only": int(view_only),
@@ -71,19 +84,62 @@ def view_desktop():
         "Desktop (View)": url_for("pwncollege_workspace.view_desktop", user=user.id, password=view_password, _external=True),
     }
 
+    if start_on_demand_service(user, "desktop") is False:
+        return render_template("iframe.html", active=False)
+
     return render_template("iframe.html",
                            iframe_name="workspace",
                            iframe_src=iframe_src,
                            share_urls=share_urls,
                            active=True)
 
+@workspace.route("/workspace/desktop-win")
+@authed_only
+def view_desktop_win():
+    user_id = request.args.get("user")
+
+    if user_id and not is_admin():
+        abort(403)
+
+    user = get_current_user() if not user_id else Users.query.filter_by(id=int(user_id)).first_or_404()
+    container = get_current_container(user)
+    if not container:
+        return render_template("iframe.html", active=False)
+
+    interact_password = container_password(container, "desktop", "interact")
+    view_password = container_password(container, "desktop", "view")
+
+    view_only = user_id is not None
+    service = "~".join(("desktop", str(user.id), container_password(container, "desktop")))
+
+    vnc_params = {
+        "autoconnect": 1,
+        "reconnect": 1,
+        "reconnect_delay": 200,
+        "resize": "remote",
+        "path": url_for("pwncollege_workspace.forward_workspace", service=service, service_path="websockify"),
+        "view_only": int(view_only),
+        "password": "abcd",
+    }
+    iframe_src = url_for("pwncollege_workspace.forward_workspace", service=service, service_path="vnc.html", **vnc_params)
+
+    if start_on_demand_service(user, "desktop") is False:
+        return render_template("iframe.html", active=False)
+
+    return render_template("iframe.html",
+                           iframe_name="workspace",
+                           iframe_src=iframe_src,
+                           share_urls={},
+                           active=True)
 
 @workspace.route("/workspace/<service>")
 @authed_only
 def view_workspace(service):
+    user = get_current_user()
     active = bool(get_current_dojo_challenge())
+    if start_on_demand_service(user, service) is False:
+        return render_template("iframe.html", active=False)
     return render_template("iframe.html", iframe_name="workspace", iframe_src=f"/workspace/{service}/", active=active)
-
 
 @workspace.route("/workspace/<service>/", websocket=True)
 @workspace.route("/workspace/<service>/<path:service_path>", websocket=True)
@@ -136,17 +192,6 @@ def forward_workspace(service, service_path=""):
 
     else:
         abort(404)
-
-    if service_name in ondemand_services:
-        try:
-            exec_run(
-                f"/run/current-system/sw/bin/dojo-{service_name}",
-                workspace_user="hacker",
-                user_id=user.id,
-                assert_success=True,
-            )
-        except docker.errors.NotFound:
-            abort(404)
 
     current_user = get_current_user()
     if user != current_user:
