da37aefe18360de8d5cdc8ed7df39906f81886b5
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-08-06 00:44:48 -0700

    ?

--- workspace/vm/windows/postinst-files/startup.ps1
+++ workspace/vm/windows/postinst-files/startup.ps1
@@ -1,4 +1,4 @@
-Remove-LocalGroupMember -Group "Administrators" -Member hacker
+# Remove-LocalGroupMember -Group "Administrators" -Member hacker
 (Get-Service WinFsp.Launcher).WaitForStatus('Running')
 & "C:\Program Files (x86)\WinFsp\bin\fsreg.bat" virtiofs "D:\virtio-win\viofs\2k22\amd64\virtiofs.exe" "-t %1 -m %2"
 & "C:\Program Files (x86)\WinFsp\bin\launchctl-x64.exe" start virtiofs viofsX flag X:
