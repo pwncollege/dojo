85a83f0e8a9a548b90e1c07c050db1e0df216c88
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-08-06 02:10:53 -0700

    add suid to windows script (#516)
    * windows vm stages
    
    * fix qemu-img invocation
    
    * add suid to windows script
    
    * run appropriate build hooks
    
    * timeout waiting for qemu
    
    * idk how to use the set constructor
    
    * never pressed save on this
    
    * fix lingering template
    
    * typo
    
    * fix old path
    
    * add back mistakenly deleted thing
    
    * add missing virtio-win-driver import
    
    * add some needed packages
    
    * silly name change
    
    * add ssh
    
    * add netcat-openbsd
    
    * ???
    
    * typo
    
    * typo
    
    * ?
    
    * ??
    
    * ??
    
    * test
    
    * revert test
    
    * tests
    
    * fix qmp
    
    * better
    
    * fixes
    
    * substitutes
    
    * typos
    
    * fixes

--- workspace/vm/windows/setup-drive.nix
+++ workspace/vm/windows/setup-drive.nix
@@ -9,9 +9,11 @@ stdenv.mkDerivation {
   dontConfigure = true;
 
   buildPhase = ''
+    runHook preBuild
     ${dosfstools}/bin/mkfs.fat -F 12 -C $out 1440
     cd $src
     ${mtools}/bin/mcopy -si $out ./Autounattend.xml ./setup.ps1 ./config_startup.ps1 ./sshd_config ::
+    runHook postBuild
   '';
 
   dontInstall = true;
--- workspace/vm/windows/windows-script.nix
+++ workspace/vm/windows/windows-script.nix
@@ -2,6 +2,10 @@
 
 let
   dojo-service = import ../../services/service.nix { inherit pkgs; };
+  windows-vm = pkgs.callPackage ./windows-vm.nix { };
+  virtio-win-drivers = pkgs.callPackage ./virtio-win-drivers.nix { };
+  setup-drive = pkgs.callPackage ./setup-drive.nix { };
+  server-iso = pkgs.callPackage ./server-iso.nix { };
 in
 pkgs.stdenv.mkDerivation {
   pname = "windows-script";
@@ -14,13 +18,19 @@ pkgs.stdenv.mkDerivation {
   dontPatchShebangs = true;
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
     substitute $src $out/bin/windows \
       --subst-var-by virtiofsd "${pkgs.virtiofsd}" \
       --subst-var-by qemu "${pkgs.qemu}" \
       --subst-var-by openssh "${pkgs.openssh}" \
       --subst-var-by coreutils "${pkgs.coreutils}" \
-      --subst-var-by dojo-service "${dojo-service}"
+      --subst-var-by dojo-service "${dojo-service}" \
+      --subst-var-by windows-vm "${windows-vm}" \
+      --subst-var-by virtio-win-drivers "${virtio-win-drivers}" \
+      --subst-var-by setup-drive "${setup-drive}" \
+      --subst-var-by server-iso "${server-iso}"
     chmod +x $out/bin/windows
+    runHook postInstall
   '';
 }
