3d9c435758e620b222070f1c8e03d402a4bd9dab
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-08-06 01:23:46 -0700

    revert test

--- workspace/vm/windows/windows-vm.nix
+++ workspace/vm/windows/windows-vm.nix
@@ -95,8 +95,9 @@ stdenv.mkDerivation {
       -chardev socket,id=mon1,host=localhost,port=4444,server=on,wait=off \
       -mon chardev=mon1 \
       -drive "file=${setup-drive},read-only=on,format=raw,index=0,if=floppy" \
-      -drive "file=${virtio-win-drivers}/share/virtio-drivers.iso,read-only=on,media=cdrom,index=1" \
-      -drive "file=$out/windows-base.qcow2,if=virtio,cache=writeback,discard=ignore,format=qcow2,index=2" &
+      -drive "file=${server-iso},read-only=on,media=cdrom,index=1" \
+      -drive "file=${virtio-win-drivers}/share/virtio-drivers.iso,read-only=on,media=cdrom,index=2" \
+      -drive "file=$out/windows-base.qcow2,if=virtio,cache=writeback,discard=ignore,format=qcow2,index=3" &
     qemu_pid="$!"
     
     # wait for SSH to open
