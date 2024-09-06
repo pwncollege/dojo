a6075de305f6400156d4a3464b2a8279676ccbd9
Connor Nelson <Connor@ConnorNelson.com>
2024-09-06 13:42:01 -0700

    Workspace: Improve `chown -R` performance

--- workspace/core/init.nix
+++ workspace/core/init.nix
@@ -54,7 +54,7 @@ let
       passwd -d root
     fi
 
-    chown -R 1000:1000 /home/hacker
+    find /home/hacker -not \( -user 1000 -and -group 1000 \) -exec chown 1000:1000 {} +
     chmod 755 /home/hacker
 
     if [ -x "/challenge/.init" ]; then
