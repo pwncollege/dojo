942bcaa5fc06d4db371f9b52dad4a7da6a3d64fc
Erik Trickel <erik.trickel@asu.edu>
2024-12-18 11:49:40 -0700

    Added grace_period, fixed a bug with extra late date and extensions, and improved due date description (#625)

--- dojo_plugin/pages/course.py
+++ dojo_plugin/pages/course.py
@@ -42,7 +42,6 @@ def assessment_name(dojo, assessment):
 def grade(dojo, users_query, *, ignore_pending=False):
     if isinstance(users_query, Users):
         users_query = Users.query.filter_by(id=users_query.id)
-
     now = datetime.datetime.now(datetime.timezone.utc)
     assessments = dojo.course.get("assessments") or []
 
@@ -50,9 +49,10 @@ def grade(dojo, users_query, *, ignore_pending=False):
     for assessment in assessments:
         if assessment["type"] not in ["checkpoint", "due"]:
             continue
+
         assessment_dates[assessment["id"]][assessment["type"]] = (
-            datetime.datetime.fromisoformat(assessment["date"]).astimezone(datetime.timezone.utc),
-            datetime.datetime.fromisoformat(assessment.get("extra_late_date","3000-01-01T16:59:59-07:00")).astimezone(datetime.timezone.utc),
+            datetime.datetime.fromisoformat(assessment["date"]).astimezone(datetime.timezone.utc) + datetime.timedelta(hours=assessment.get("grace_period",0)),
+            datetime.datetime.fromisoformat(assessment.get("extra_late_date","3000-01-01T16:59:59-07:00")).astimezone(datetime.timezone.utc) + datetime.timedelta(hours=assessment.get("grace_period",0)),
             assessment.get("extensions") or {},
         )
 
@@ -64,21 +64,34 @@ def grade(dojo, users_query, *, ignore_pending=False):
                 if date_type not in assessment_dates[module_id]:
                     return None
                 date, extra_late_date, extensions = assessment_dates[module_id][date_type]
+
+                # if dojo creator made a mistake and date comes after extra late date then fast forward extra late date to due date + 1 otherwise it will double count the solve
+                if date > extra_late_date:
+                    extra_late_date = date + datetime.timedelta(milliseconds=1)
+
                 if label == "extra_late_solves":
                     if extra_late_date is None:
                         return False
                     date = extra_late_date
-                user_date = db.case(
+                
+                extension_adjusted_date = db.case(
                     [(Solves.user_id == int(user_id), date + datetime.timedelta(days=days))
                      for user_id, days in extensions.items()],
                     else_=date
                 ) if extensions else date
-                if label == "late_solves":
 
-                    return and_(Solves.date >= user_date, Solves.date < extra_late_date)
+                extension_adj_extra_late_date = db.case(
+                    [(Solves.user_id == int(user_id), extra_late_date + datetime.timedelta(days=days))
+                     for user_id, days in extensions.items()],
+                    else_=extra_late_date
+                ) if extensions else extra_late_date
+
+                if label == "late_solves":
+                    return and_(Solves.date >= extension_adjusted_date, Solves.date < extension_adj_extra_late_date) # after due date but before adjusted extra late date
                 elif label == "extra_late_solves":
-                    return Solves.date >= user_date
-                return Solves.date < user_date
+                    return Solves.date >= extension_adj_extra_late_date  # after extra late date
+                else:
+                    return Solves.date < extension_adjusted_date  # before due date
         return db.func.sum(
             db.case([(DojoModules.id == module_id, cast(query(module_id), db.Integer))
                      for module_id in assessment_dates] +
@@ -254,9 +267,10 @@ def grade(dojo, users_query, *, ignore_pending=False):
                 elif late_solves and not extra_late_solves:
                     progress = f"{due_solves} (+{late_solves}) / {challenge_count_required}"
                 elif not late_solves and extra_late_solves:
-                    progress = f"{due_solves} (+{extra_late_solves}) / {challenge_count_required}"
+                    progress = f"{due_solves} (+0,+{extra_late_solves}) / {challenge_count_required}"
                 else:
-                    progress = f"{due_solves} (+{late_solves}) (+{extra_late_solves}) / {challenge_count_required}"
+                    progress = f"{due_solves} (+{late_solves},+{extra_late_solves}) / {challenge_count_required}"
+
                 if override is None:
                     late_points = late_value * capped_late_solves
                     extra_late_points = extra_late_value * capped_extra_late_solves
