9f75e8b72e76853a566a3c9ca02828a56aba644c
Connor Nelson <Connor@ConnorNelson.com>
2025-01-03 13:59:04 -0700

    Challenge: Remove challenge image

--- challenge/docker-entrypoint.d/41_fix_ipython_autocomplete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p /home/hacker/.ipython/profile_default/startup
-cat >/home/hacker/.ipython/profile_default/startup/22-autocomplete.py <<EOF
-import IPython
-py = IPython.get_ipython()
-py.Completer.use_jedi = False
-py.Completer.greedy = True
-EOF
