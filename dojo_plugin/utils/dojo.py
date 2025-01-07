1f0b1f22c971edd5d883632fb2346d9c6f8b8535
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2025-01-07 16:37:19 -0500

    Fix symlink check and revert repo on fail
    Co-authored-by: Yan Shoshitaishvili <yans@yancomm.net>

--- dojo_plugin/utils/dojo.py
+++ dojo_plugin/utils/dojo.py
@@ -412,6 +412,12 @@ def dojo_yml_dir(spec):
         do.write(spec)
     return yml_dir
 
+def _assert_no_symlinks(dojo_dir):
+    if not isinstance(dojo_dir, pathlib.Path):
+        dojo_dir = pathlib.Path(dojo_dir)
+    for path in dojo_dir.rglob("*"):
+        assert dojo_dir == path or dojo_dir in path.resolve().parents, f"Error: symlink `{path}` references path outside of the dojo"
+
 def dojo_clone(repository, private_key):
     tmp_dojos_dir = DOJOS_TMP_DIR
     tmp_dojos_dir.mkdir(exist_ok=True)
@@ -432,15 +438,20 @@ def dojo_clone(repository, private_key):
                    check=True,
                    capture_output=True)
 
+    _assert_no_symlinks(clone_dir)
+
     return clone_dir
 
 
-def dojo_git_command(dojo, *args):
+def dojo_git_command(dojo, *args, repo_path=None):
     key_file = tempfile.NamedTemporaryFile("w")
     key_file.write(dojo.private_key)
     key_file.flush()
 
-    return subprocess.run(["git", "-C", str(dojo.path), *args],
+    if repo_path is None:
+        repo_path = str(dojo.path)
+
+    return subprocess.run(["git", "-C", repo_path, *args],
                           env={
                               "GIT_SSH_COMMAND": f"ssh -i {key_file.name}",
                               "GIT_TERMINAL_PROMPT": "0",
@@ -451,9 +462,24 @@ def dojo_git_command(dojo, *args):
 
 def dojo_update(dojo):
     if dojo.path.exists():
-        dojo_git_command(dojo, "fetch", "--depth=1", "origin")
-        dojo_git_command(dojo, "reset", "--hard", "origin")
-        dojo_git_command(dojo, "submodule", "update", "--init", "--recursive")
+        old_commit = dojo_git_command(dojo, "rev-parse", "HEAD").stdout.decode().strip()
+
+        tmp_dir = tempfile.TemporaryDirectory(dir=DOJOS_TMP_DIR)
+
+        os.rename(str(dojo.path), tmp_dir)
+
+        dojo_git_command(dojo, "fetch", "--depth=1", "origin", repo_path=tmp_dir)
+        dojo_git_command(dojo, "reset", "--hard", "origin", repo_path=tmp_dir)
+        dojo_git_command(dojo, "submodule", "update", "--init", "--recursive", repo_path=tmp_dir)
+
+        try:
+            _assert_no_symlinks(tmp_dir)
+        except AssertionError:
+            dojo_git_command(dojo, "reset", "--hard", old_commit, repo_path=tmp_dir)
+            dojo_git_command(dojo, "submodule", "update", "--init", "--recursive", repo_path=tmp_dir)
+            raise
+        finally:
+            os.rename(tmp_dir, str(dojo.path))
     else:
         tmpdir = dojo_clone(dojo.repository, dojo.private_key)
         os.rename(tmpdir.name, str(dojo.path))
