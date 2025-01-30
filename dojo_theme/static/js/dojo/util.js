48ff2d0e660dc4954683d5c1ca458d75085f85ea
Connor Nelson <Connor@ConnorNelson.com>
2025-01-30 23:43:16 +0000

    Site: Temporarily support old theme

--- dojo_theme/static/js/dojo/util.js
+++ dojo_theme/static/js/dojo/util.js
@@ -11,3 +11,18 @@ function copyToClipboard(event) {
         }, 1000);
     })
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (new Date() >= new Date("2025-03-01")) return;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const theme = urlParams.get("theme");
+
+    if (theme) {
+        document.body.classList.add(`theme-${theme}`);
+        localStorage.setItem('theme', theme);
+    } else {
+        const storedTheme = localStorage.getItem('theme');
+        document.body.classList.add(`theme-${storedTheme}`);
+    }
+});
