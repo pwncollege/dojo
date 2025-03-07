dd5c085f16d9323d0f88dee3b4e3b551fa74e155
Connor Nelson <Connor@ConnorNelson.com>
2025-03-07 13:35:12 -0700

    Course: Support locally grading all students (#670)

--- dojo_theme/static/js/dojo/grades.js
+++ dojo_theme/static/js/dojo/grades.js
@@ -1,18 +1,31 @@
-async function workerModule() {
+async function gradeWorkerModule() {
     importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.3/full/pyodide.js");
 
     const pyodide = await loadPyodide();
 
     self.onmessage = async (event) => {
-        const { code, data } = event.data;
-        await pyodide.loadPackagesFromImports(code);
-        try {
-            await pyodide.runPythonAsync(code);
-            const grade = pyodide.globals.get("grade");
-            const result = JSON.parse(JSON.stringify(grade(pyodide.toPy(data)).toJs()));
-            self.postMessage({ type: "result", result });
-        } catch (err) {
-            self.postMessage({ type: "error", error: err.toString() });
+        const type = event.data.type;
+
+        if (event.data.type === "load") {
+            const { id, code } = event.data;
+            try {
+                await pyodide.loadPackagesFromImports(event.data.code);
+                await pyodide.runPythonAsync(code);
+                self.postMessage({ id, type: "loaded" });
+            } catch (err) {
+                self.postMessage({ id, type: "error", error: err.toString() });
+            }
+        }
+
+        if (event.data.type === "grade") {
+            const { id, data } = event.data;
+            try {
+                const grade = pyodide.globals.get("grade");
+                const grades = JSON.parse(JSON.stringify(grade(pyodide.toPy(data)).toJs()));
+                self.postMessage({ id, type: "graded", grades });
+            } catch (err) {
+                self.postMessage({ id, type: "error", error: err.toString() });
+            }
         }
     };
 
@@ -20,24 +33,67 @@ async function workerModule() {
 }
 
 
-function renderGrades(gradesData) {
-    const grades = document.getElementById("grades");
-    grades.innerHTML = "";
+function createWorker(workerModule) {
+    const sandboxedWorkerURL = `data:application/javascript;base64,${btoa("(" + workerModule.toString() + ")()")}`;
+    const worker = new Worker(sandboxedWorkerURL);
+
+    worker.onmessage = (event) => {
+        if (event.data.type === "error") {
+            console.error(event.data.error);
+        }
+    };
+
+    worker.waitForMessage = (expectedType) =>
+        new Promise((resolve) => {
+          const handler = (event) => {
+            if (event.data.type === expectedType) {
+              worker.removeEventListener("message", handler);
+              resolve(event.data);
+            }
+          };
+          worker.addEventListener("message", handler);
+        });
+
+    return worker;
+}
+
+
+async function loadGrades(selector) {
+    const gradeWorker = createWorker(gradeWorkerModule);
+
+    const gradeCodePromise = fetch(`/${init.dojo}/grade.py`).then(response => response.text());
+    const coursePromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/course`).then(response => response.json());
+    const modulesPromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/modules`).then(response => response.json());
+    const solvesPromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/solves`).then(response => response.json());
+
+    await waitForMessage("ready");
+    const gradeCode = await gradeCodePromise;
+
+    gradeWorker.postMessage({ type: "load", code: gradeCode });
+    await waitForMessage("loaded");
+
+    const [courseData, modulesData, solvesData] = await Promise.all([coursePromise, modulesPromise, solvesPromise])
+    gradeWorker.postMessage({ type: "grade", data: { course: courseData.course, modules: modulesData.modules, solves: solvesData.solves } });
+
+    const gradesData = (await waitForMessage("graded")).grades;
+
+    const gradesElement = document.querySelector(selector)
+    gradesElement.innerHTML = "";
 
     const h3 = document.createElement("h3");
-    const gradeCode = document.createElement("code");
-    gradeCode.textContent = gradesData.overall.letter;
-    gradeCode.style.fontSize = "2em";
+    const letterGrade = document.createElement("code");
+    letterGrade.textContent = gradesData.overall.letter;
+    letterGrade.style.fontSize = "2em";
     h3.append(
         document.createTextNode("Your current grade in the class: "),
-        gradeCode,
+        letterGrade,
         document.createTextNode(` (${(gradesData.overall.credit * 100).toFixed(2)}%)`)
     );
-    grades.appendChild(h3);
+    gradesElement.appendChild(h3);
 
     const table = document.createElement("table");
     table.classList.add("table", "table-striped");
-    grades.appendChild(table);
+    gradesElement.appendChild(table);
 
     const thead = document.createElement("thead");
     const headerRow = document.createElement("tr");
@@ -65,46 +121,29 @@ function renderGrades(gradesData) {
     table.appendChild(tbody);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    // TODO: Remove feature flag when this feature is ready
-    if (!window.location.search.includes("local"))
-        return;
 
-    const sandboxedWorkerURL = `data:application/javascript;base64,${btoa("(" + workerModule.toString() + ")()")}`;
-    const worker = new Worker(sandboxedWorkerURL);
+async function loadAllGrades(selector) {
+    const gradeWorker = createWorker(gradeWorkerModule);
 
-    worker.onmessage = (event) => {
-        if (event.data.type === "error") {
-            console.error(event.data.error);
-        }
+    const gradeCodePromise = fetch(`/${init.dojo}/grade.py`).then(response => response.text());
+    const coursePromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/course`).then(response => response.json());
+    const modulesPromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/modules`).then(response => response.json());
+    const solvesPromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/course/solves`).then(response => response.json());
+    const studentsPromise = fetch(`/pwncollege_api/v1/dojos/${init.dojo}/course/students`).then(response => response.json());
 
-        if (event.data.type === "result") {
-            if (!event.data.result)
-                return;
-            renderGrades(event.data.result);
-        }
-    };
+    await waitForMessage("ready");
+    const gradeCode = await gradeCodePromise;
 
-    const workerReadyPromise = new Promise((resolve) => {
-        const onMessageHandler = (event) => {
-            if (event.data.type === "ready") {
-                resolve();
-                worker.removeEventListener("message", onMessageHandler);
-            }
-        };
-        worker.addEventListener("message", onMessageHandler);
+    gradeWorker.postMessage({ type: "load", code: gradeCode });
+    await waitForMessage("loaded");
+
+    const [courseData, modulesData, solvesData, studentsData] = await Promise.all([coursePromise, modulesPromise, solvesPromise, studentsPromise])
+
+    const grades = {};
+    Object.entries(studentsData.students).forEach(async ([studentToken, student]) => {
+        const course = { ...courseData.course, student: {token: studentToken, ...student} };
+        const solves = solvesData.solves.filter(solve => solve.student_token === studentToken);
+        gradeWorker.postMessage({ type: "grade", data: { course, modules: modulesData.modules, solves } });
+        grades[studentToken] = (await waitForMessage("graded")).grades;
     });
-
-    Promise.all([
-        workerReadyPromise,
-        fetch(`/${init.dojo}/grade.py`).then(response => response.text()),
-        fetch(`/pwncollege_api/v1/dojos/${init.dojo}/modules`).then(response => response.json()),
-        fetch(`/pwncollege_api/v1/dojos/${init.dojo}/solves`).then(response => response.json()),
-        fetch(`/pwncollege_api/v1/dojos/${init.dojo}/course`).then(response => response.json())
-    ]).then(([_, code, modulesData, solvesData, courseData]) => {
-        const data = { modules: modulesData.modules, solves: solvesData.solves, course: courseData.course };
-        worker.postMessage({ code, data });
-    }).catch(error => {
-        console.error("Error:", error);
-    });
-});
+}
