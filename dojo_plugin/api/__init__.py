62c83f6a4faf51fbf24a73953b5002091a0ad79e
Connor Nelson <Connor@ConnorNelson.com>
2025-03-07 09:51:40 -0700

    Site: Remove bootstrap api

--- dojo_plugin/api/__init__.py
+++ dojo_plugin/api/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint, current_app
 from flask_restx import Api
 
 from .v1.belts import belts_namespace
-from .v1.bootstrap import bootstrap_namespace
 from .v1.discord import discord_namespace
 from .v1.docker import docker_namespace
 from .v1.dojos import dojos_namespace
@@ -17,7 +16,6 @@ api = Blueprint("pwncollege_api", __name__)
 
 api_v1 = Api(api, version="v1", doc=current_app.config.get("SWAGGER_UI"))
 api_v1.add_namespace(belts_namespace, "/belts")
-api_v1.add_namespace(bootstrap_namespace, "/bootstrap")
 api_v1.add_namespace(discord_namespace, "/discord")
 api_v1.add_namespace(docker_namespace, "/docker")
 api_v1.add_namespace(dojos_namespace, "/dojos")
