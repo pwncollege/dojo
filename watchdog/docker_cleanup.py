9bb7d2d58c4741383f80a9d72fddee1c4cf0464b
Connor Nelson <Connor@ConnorNelson.com>
2025-03-10 17:01:14 -0700

    Watchdog: Increase image prune timeout

--- watchdog/docker_cleanup.py
+++ watchdog/docker_cleanup.py
@@ -15,30 +15,36 @@ logging.info("Starting docker cleanup")
 
 workspace_nodes = json.load(open("/var/workspace_nodes.json"))
 workspace_node_ips = [f"192.168.42.{int(node_id) + 1}" for node_id in workspace_nodes]
-docker_clients = [docker.DockerClient(base_url=f"tcp://{node_ip}:2375") for node_ip in workspace_node_ips]
 
 now = datetime.now(timezone.utc)
 
-def cleanup(docker_client):
+def remove_old_containers(docker_client_ip):
+    docker_client = docker.DockerClient(base_url=f"tcp://{docker_client_ip}:2375")
     containers = docker_client.containers.list(filters={"label": "dojo.user_id"}, ignore_removed=True)
     container_is_old = lambda container: (now - datetime.fromisoformat(container.attrs["Created"].replace("Z", "+00:00"))) > OLD_CONTAINER_AGE
     old_containers = [container for container in containers if container_is_old(container)]
 
-    def remove_container(container):
+    for container in old_containers:
         user_id = container.labels["dojo.user_id"]
-        logging.info(f"Removing docker container {container.id} (user {user_id}) on {docker_client.api.base_url}")
+        logging.info(f"Removing old docker container {container.id} (user {user_id}) on {docker_client.api.base_url}")
         container.remove(force=True)
-        container.wait(condition="removed")
-        logging.info(f"Removed docker container {container.id} (user {user_id}) on {docker_client.api.base_url}")
 
-    with ThreadPoolExecutor() as executor:
-        list(executor.map(remove_container, old_containers))
+    logging.info(f"Removed old docker containers on {docker_client.api.base_url}")
 
+with ThreadPoolExecutor() as executor:
+    logging.info("Removing old containers")
+    list(executor.map(remove_old_containers, workspace_node_ips))
+    logging.info("Removed old containers")
+
+def prune_images(docker_client_ip):
+    docker_client = docker.DockerClient(base_url=f"tcp://{docker_client_ip}:2375", timeout=3600)
     logging.info(f"Pruning docker images on {docker_client.api.base_url}")
     docker_client.images.prune()
     logging.info(f"Prune docker images complete on {docker_client.api.base_url}")
 
 with ThreadPoolExecutor() as executor:
-    list(executor.map(cleanup, docker_clients))
+    logging.info("Pruning images")
+    list(executor.map(prune_images, workspace_node_ips))
+    logging.info("Pruned images")
 
 logging.info("Completed docker cleanup")
