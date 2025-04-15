2a8cf5ed36841774e1f47acf59a38823da070e33
Max Trapido <maxtrapido@gmail.com>
2025-04-15 10:41:37 -0700

    Add testing for unlocking a locked challenge

--- test/test_running.py
+++ test/test_running.py
@@ -533,8 +533,8 @@ def test_progression_locked(progression_locked_dojo, random_user):
     assert session.get(f"{DOJO_URL}/dojo/{progression_locked_dojo}/join/").status_code == 200
     start_challenge(progression_locked_dojo, "progression-locked-module", "unlocked-challenge", session=session)
 
-    with pytest.raises(AssertionError) as error:
+    with pytest.raises(AssertionError, match="Failed to start challenge: This challenge is locked"):
         start_challenge(progression_locked_dojo, "progression-locked-module", "locked-challenge", session=session)
 
-    print(f"I don't know what I'm doing so I'm just gonna print this error {error}")
-    assert False
+    solve_challenge(progression_locked_dojo, "progression-locked-module", "unlocked-challenge", session=session, user=uid)
+    start_challenge(progression_locked_dojo, "progression-locked-module", "locked-challenge", session=session)
