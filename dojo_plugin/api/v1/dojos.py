0583491cc3fddc1b2dba041057461598c6fdd735
Connor Nelson <Connor@ConnorNelson.com>
2025-05-29 15:59:34 -0700

    Refactor

--- dojo_plugin/api/v1/dojos.py
+++ dojo_plugin/api/v1/dojos.py
@@ -283,21 +283,15 @@ class DojoCourseSolveList(Resource):
 class ChallengeResource(Resource):
     @authed_only
     @dojo_route
-    def get(self, dojo, module_id, challenge_id):
+    def get(self, dojo, module, challenge_id):
         user = get_current_user()
-
-        module = next((m for m in dojo.modules if m.id == module_id), None)
-
-        if module is None:
-            return {"success": False, "error": "Invalid module id"}, 404
-        
 
         dojo_challenge = next((c for c in module.visible_challenges() if c.id == challenge_id), None)
 
         if dojo_challenge is None:
             return {"success": False, "error": "Invalid challenge id"}, 404
 
-        
+
         if all((dojo_challenge.progression_locked, dojo_challenge.challenge_index != 0, not dojo.is_admin())):
             previous_dojo_challenge = dojo_challenge.module.challenges[dojo_challenge.challenge_index - 1]
             is_challenge_unlocked = (Solves.query.filter_by(user=user, challenge=dojo_challenge.challenge).first() or
@@ -307,7 +301,7 @@ class ChallengeResource(Resource):
                     "success": False,
                     "error": "This challenge is locked"
                 }, 403
-        
+
         return {
             "success": True,
             "description": render_markdown(dojo_challenge.description)
