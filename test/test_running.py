c8ca8d95d967a4a7c120dc9e3b34df20993fdb05
Yan <yans@yancomm.net>
2025-07-24 10:19:03 -0700

    sigh

--- test/test_running.py
+++
@@ -1,9 +0,0 @@
-# This file has been refactored - tests have been moved to separate files:
-# - test_auth.py: Authentication tests
-# - test_dojos.py: Dojo management tests  
-# - test_challenges.py: Challenge tests
-# - test_workspace.py: Workspace tests
-# - test_scoreboard.py: Scoreboard tests
-# - test_belts.py: Belt system tests
-# - test_surveys.py: Survey tests
-# - test_search.py: Search tests
