7a967901fa6f4845f4b5deaacd0fa394063e3cb1
Theodor Kitzenmaier <tkitzenm@asu.edu>
2025-07-30 15:08:01 -0700

    Workspace: Test
    Don't abuse formatting.

--- test/test_welcome.py
+++ test/test_welcome.py
@@ -169,7 +169,9 @@ def test_welcome_practice(random_user_browser, welcome_dojo):
         vs.send_keys("/challenge/solve < secret | tee /tmp/out\n")
         time.sleep(10)
         flag = workspace_run("tail -n1 /tmp/out 2>&1", user=random_id).stdout.split()[-1]
-        print(f"Hey look at this -> {workspace_run("tail -n1 /tmp/out 2>&1", user=random_id).stdout}")
-        print(f"Hey look at this -> {workspace_run("tail -n1 secret 2>&1", user=random_id).stdout}")
+        temp = workspace_run("tail -n1 /tmp/out 2>&1", user=random_id).stdout
+        secret = workspace_run("tail -n1 secret 2>&1", user=random_id).stdout
+        print(f"Hey look at this -> {temp}")
+        print(f"Hey look at this -> {secret}")
     challenge_submit(browser, idx, flag)
     browser.close()
