e590f2a7e060bf2707c206544f4e0da0e9b1fc84
Theodor Kitzenmaier <tkitzenm@asu.edu>
2025-08-12 12:04:02 -0700

    Workspace: Actionbar refactor
    Separated actionbar into its own component.
    
    Moved JS for actionbar to its own file.
    -	Reordered functions to correspond with their caller's position in the actionbar.
    -	Removed window.parent.[function] jank.

---
+++ dojo_theme/static/js/dojo/actionbar.js
@@ -0,0 +1,209 @@
+function selectService(service) {
+    const content = document.getElementById("workspace-content");
+    const url = new URL("/pwncollege_api/v1/workspace", window.location.origin);
+    url.searchParams.set("service", service);
+    fetch(url, {
+        method: "GET",
+        credentials: "same-origin"
+    })
+    .then(response => response.json())
+    .then(result => {
+        content.src = result["iframe_src"];
+    });
+}
+
+function animateBanner(message, type) {
+    const color = {
+        success: "var(--brand-green)",
+        error:   "var(--error)",
+        warn:    "var(--warn)"
+    }[type] ?? "var(--warn)";
+    const animation = type === "success" ? "animate-banner" : "animate-banner-fast";
+
+    $("#workspace-notification-banner").removeClass("animate-banner animate-banner-fast");
+    $("#workspace-notification-banner").offsetHeight;  // Force reflow of element to play animation again.
+    $("#workspace-notification-banner")
+      .html(message)
+      .css("border-color", color)
+      .addClass(animation);
+}
+
+function submitFlag(flag) {
+    var body = {
+        'challenge_id': parseInt(document.getElementById("current-challenge-id").value),
+        'submission': flag,
+    };
+    var params = {};
+
+    CTFd.api.post_challenge_attempt(params, body)
+    .then(function (response) {
+        const challengeName = $("#current-challenge-id").attr("data-challenge-name");
+        if (response.data.status == "incorrect") {
+            animateBanner("Incorrect!", "error");
+        }
+        else if (response.data.status == "correct") {
+            animateBanner(`&#127881 Successfully completed <b>${challengeName}</b>! &#127881`, "success");
+        }
+        else if (response.data.status == "already_solved") {
+            animateBanner(`&#127881 Solved <b>${challengeName}</b>! &#127881`, "success");
+        }
+        else {
+            animateBanner("Submission Failed.", "warn");
+        }
+    });
+}
+
+function startChallenge() {
+    const privileged = $("#workspace-change-privilege").attr("data-privileged") === "true";
+
+    CTFd.fetch("/pwncollege_api/v1/docker", {
+        method: "GET",
+        credentials: 'same-origin'
+    }).then(function (response) {
+        if (response.status === 403) {
+            // User is not logged in or CTF is paused.
+            window.location =
+                CTFd.config.urlRoot +
+                "/login?next=" +
+                CTFd.config.urlRoot +
+                window.location.pathname +
+                window.location.hash;
+        }
+        return response.json();
+    }).then(function (result) {
+        if (result.success == false) {
+            return;
+        }
+
+        var params = {
+            "dojo": result.dojo,
+            "module": result.module,
+            "challenge": result.challenge,
+            "practice": privileged,
+        };
+
+        CTFd.fetch('/pwncollege_api/v1/docker', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        }).then(function (response) {
+            return response.json;
+        }).then(function (result) {
+            if (result.success == false) {
+                return;
+            }
+
+            selectService($("#workspace-select").val());
+
+            $(".btn-challenge-start")
+            .removeClass("disabled")
+            .removeClass("btn-disabled")
+            .prop("disabled", false);
+        })
+    });
+}
+
+function challengeStartCallback(event) {
+    event.preventDefault();
+
+    $(".btn-challenge-start")
+    .addClass("disabled")
+    .addClass("btn-disabled")
+    .prop("disabled", true);
+
+    if (document.getElementById("challenge-restart").contains(event.target)) {
+        startChallenge();
+    }
+    else if (document.getElementById("workspace-change-privilege") != null && document.getElementById("workspace-change-privilege").contains(event.target)) {
+        $("#workspace-change-privilege").attr("data-privileged", (_, v) => v !== "true");
+        displayPrivileged(false);
+        startChallenge();
+    }
+    else {
+        console.log("Failed to start challenge.");
+
+        $(".btn-challenge-start")
+        .removeClass("disabled")
+        .removeClass("btn-disabled")
+        .prop("disabled", false);
+    }
+}
+
+function displayPrivileged(invert) {
+    const button = $("#workspace-change-privilege");
+    const privileged = button.attr("data-privileged") === "true";
+    const lockStatus = privileged === invert;
+
+    button.find(".fas")
+        .toggleClass("fa-lock", lockStatus)
+        .toggleClass("fa-unlock", !lockStatus);
+
+    button.attr("title", privileged ? "Restart unprivileged"
+                                    : "Restart privileged");
+}
+
+function loadWorkspace() {
+    var previousWorkspace = localStorage.getItem("previousWorkspace");
+    var workspaceSelect = document.getElementById("workspace-select");
+    var option = workspaceSelect.options[0];
+    if (previousWorkspace && workspaceSelect) {
+        for (var i = 0; i < workspaceSelect.options.length; i++) {
+            if (workspaceSelect.options[i].text === previousWorkspace) {
+                option = workspaceSelect.options[i];
+                option.selected = true;
+                break;
+            }
+        }
+    }
+    selectService(option.value);
+}
+
+$(() => {
+    if (new URLSearchParams(window.location.search).has("hide-navbar")) {
+        hideNavbar();
+    }
+    if (new URLSearchParams(window.location.search).has("hide-actionbar")) {
+        $("#workspace-controls").hide();
+    }
+    $("footer").hide();
+
+    loadWorkspace();
+    $("#workspace-select").change((event) => {
+        event.preventDefault();
+        localStorage.setItem("previousWorkspace", event.target.options[event.target.selectedIndex].text);
+        selectService(event.target.value);
+    });
+
+    $("#flag-input").on("input", function(event) {
+        event.preventDefault();
+        if ($(this).val().match(/pwn.college{.*}/)) {
+            submitFlag($(this).val());
+        }
+    });
+    $("#flag-input").on("keypress", function(event) {
+        if (event.key === "Enter" || event.keyCode === 13) {
+            submitFlag($(this).val());
+        }
+    });
+
+    $(".btn-challenge-start").click(challengeStartCallback);
+
+    if ($("#workspace-change-privilege").length) {
+        $("#workspace-change-privilege").on("mouseenter", function(event) {
+            displayPrivileged(true);
+        }).on("mouseleave", function(event) {
+            displayPrivileged(false);
+        });
+    }
+
+    $("#fullscreen").click((event) => {
+        event.preventDefault();
+        $("#fullscreen i").toggleClass("fa-compress fa-expand");
+        // Pages using the actionbar should implement their own fullscreen.
+        doFullscreen();
+    })
+});
