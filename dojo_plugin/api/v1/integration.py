c7557240644d3d7071106441f67ad7b7824ad599
Theodor Kitzenmaier <tkitzenm@asu.edu>
2025-09-30 14:16:54 -0700

    Updated container authentication method

--- dojo_plugin/api/v1/integration.py
+++ dojo_plugin/api/v1/integration.py
@@ -4,6 +4,7 @@ from flask import request, session
 from flask_restx import Namespace, Resource
 from CTFd.plugins import bypass_csrf_protection
 from CTFd.models import Users, Solves
+from ...utils import validate_user_container
 
 logger = logging.getLogger(__name__)
 
@@ -21,32 +22,15 @@ def authenticate_application(token):
 
 # Authentication of the internal dojo cli application.
 def authenticate_container(token):
-    docker_client = docker.DockerClient(base_url="unix://var/run/docker.sock")
-
-    container = None
     try:
-        for c in docker_client.containers.list():
-            if c.labels.get("dojo.auth_token") == token:
-                container = c
-                break
-    except Exception as e:
-        logger.error(f"Exception while listing containers: {e}")
-        return None, ({
-            "success": False,
-            "error": "Authentication processed failed"
-            }, 500)
-
-    if not container:
+        user_id = validate_user_container(token)
+        user = Users.query.filter_by(id=user_id).one()
+        return user, None
+    except:
         return None, ({
             "success": False,
             "error": "Invalid container authentication token",
             }, 401)
-
-    user_id = int(container.labels.get("dojo.as_user_id"))
-    session["id"] = user_id
-    user = Users.query.filter_by(id=user_id).one()
-
-    return user, None
 
 def authenticate(token, type):
     if not token:
