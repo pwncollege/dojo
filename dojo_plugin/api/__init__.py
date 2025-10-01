7112c54e50f1e67cd422829b31109d1061b2f6f5
dlock <hossam.saraya@gmail.com>
2025-10-02 00:24:17 +0300

    Redesign: Backend changes -- Authentication endpoints + CORS handling (#930)
    * Backend
    
    * backend: return unified_items in dojo modules endpoint
    
    * workspace: theming
    
    * - Move envs to dojo-init and config
    - Bring back challenges, modules besude unified items
    - Update dojo  endpoint json response schema
    
    * fix: remove deploy-local
    
    * revert: workspace changes

--- dojo_plugin/api/__init__.py
+++ dojo_plugin/api/__init__.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, current_app
+import os
+from flask import Blueprint, current_app, make_response, request
+from ...dojo_plugin import config
 from flask_restx import Api
 
 from ..utils.request_logging import log_exception
+from .v1.auth import auth_namespace
 from .v1.belts import belts_namespace
 from .v1.discord import discord_namespace
 from .v1.docker import docker_namespace
@@ -14,8 +17,34 @@ from .v1.workspace_tokens import workspace_tokens_namespace
 from .v1.workspace import workspace_namespace
 from .v1.search import search_namespace
 from .v1.test_error import test_error_namespace
+from .v1.user import user_namespace
 
 api = Blueprint("pwncollege_api", __name__)
+
+# Get CORS origin from environment variable
+cors_origin = config.CORS_ORIGINS
+
+# Add CORS headers to all responses only if CORS origin is set
+if cors_origin:
+    @api.after_request
+    def after_request(response):
+        response.headers['Access-Control-Allow-Origin'] = cors_origin
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
+
+    # Handle OPTIONS preflight requests
+    @api.before_request
+    def handle_preflight():
+        if request.method == "OPTIONS":
+            response = make_response()
+            response.headers['Access-Control-Allow-Origin'] = cors_origin
+            response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+            response.headers['Access-Control-Allow-Credentials'] = 'true'
+            response.headers['Access-Control-Max-Age'] = '3600'
+            return response
 
 
 api_v1 = Api(api, version="v1", doc=current_app.config.get("SWAGGER_UI"))
@@ -25,6 +54,9 @@ def handle_api_exception(error):
     log_exception(error, event_type="api_exception")
     raise
 
+
+api_v1.add_namespace(auth_namespace, "/auth")
+api_v1.add_namespace(user_namespace, "/users")
 api_v1.add_namespace(belts_namespace, "/belts")
 api_v1.add_namespace(discord_namespace, "/discord")
 api_v1.add_namespace(docker_namespace, "/docker")
