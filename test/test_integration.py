bc59173bd92b28e1ec73f63214bfe58822330633
Theodor Kitzenmaier <tkitzenm@asu.edu>
2025-10-06 13:04:20 -0700

    Start Container Before Tests
    Start challenge containers before integration tests. To get a container token, there needs to be a container.

--- test/test_integration.py
+++ test/test_integration.py
@@ -5,6 +5,17 @@ import subprocess
 
 from utils import DOJO_API, workspace_run, get_user_id, start_challenge, get_outer_container_for
 
+def container_running(func):
+    """
+    Starts the user's container with the practice welcome challenge.
+    """
+    def wrapper(*args, **kwargs):
+        _, session = kwargs["random_user"]
+        start_challenge("welcome", "welcome", "practice", practice=False, session=session)
+
+        return func(*args, **kwargs)
+    return wrapper
+
 def container_info(user):
     container_name = f"user_{get_user_id(user)}"
     outer_container = get_outer_container_for(container_name)
@@ -20,8 +31,9 @@ def container_info(user):
 def container_token(user):
     return workspace_run("cat /run/dojo/var/auth_token", user=user).stdout.strip()
 
-def test_int_auth(random_user_name):
-    name = random_user_name
+@container_running
+def test_int_auth(random_user):
+    name, _ = random_user
     token = container_token(name)
     id = get_user_id(name)
 
@@ -31,6 +43,7 @@ def test_int_auth(random_user_name):
     assert result.json()["success"], f"Container authentication request failed: ({result.status_code}) {str(result.json())}"
     assert result.json()["user_id"] == id, f"User ID mismatch, expected {id}, got {result.json()["user_id"]}"
 
+@container_running
 def test_int_submit_current(random_user):
     name, session = random_user
     token = container_token(name)
@@ -61,6 +74,7 @@ def test_int_submit_current(random_user):
     assert result.json()["success"], f"Flag submission failed: ({result.status_code}) {str(result.json())}"
     assert result.json()["status"] == "already_solved", f"Expected challenge to already be solved: ({result.status_code}) {str(result.json())}"
 
+@container_running
 def test_int_submit_other(random_user):
     name, session = random_user
     token = container_token(name)
@@ -101,15 +115,19 @@ def starting_test(name, privileged):
     assert labels["dojo.challenge_id"] == challenge, f"Challenge id mismatch, expected {challenge} but got {labels["dojo.challenge_id"]}"
     assert labels["dojo.mode"] == "privileged" if privileged else "standard", f"Privilege mismatch, expected {"privileged" if privileged else "standard"} but got {labels["dojo.mode"]}"
 
-def test_start(random_user_name):
-    starting_test(random_user_name, False)
+@container_running
+def test_start(random_user):
+    name, _ = random_user
+    starting_test(name, False)
 
-def test_privileged(random_user_name):
-    starting_test(random_user_name, True)
+@container_running
+def test_privileged(random_user):
+    name, _ = random_user
+    starting_test(name, True)
 
-def restarting_test(user, swap):
-    token = container_token(user)
-    start_info = container_info(user)
+def restarting_test(name, swap):
+    token = container_token(name)
+    start_info = container_info(name)
     practice = start_info["Config"]["Labels"]["dojo.mode"] == "privileged"
     practice = practice ^ swap
     modeStr = "privileged" if practice else "standard"
@@ -120,22 +138,25 @@ def restarting_test(user, swap):
     assert result.status_code == 200, f"Restarting challenge failed: ({result.status_code}) {str(result.json())}"
     assert result.json()["success"], f"Restarting challenge failed: ({result.status_code}) {str(result.json())}"
 
-    restart_info = container_info(user)
+    restart_info = container_info(name)
     assert restart_info["State"]["StartedAt"] != start_info["State"]["StartedAt"], "Expected new container start time"
     assert restart_info["Config"]["Labels"]["dojo.mode"] == modeStr, f"Expected dojo mode to be {modeStr} but got {restart_info["Config"]["Labels"]["dojo.mode"]}"
     assert restart_info["Config"]["Labels"]["dojo.dojo_id"] == start_info["Config"]["Labels"]["dojo.dojo_id"], f"Expected dojo to be {start_info["Config"]["Labels"]["dojo.dojo_id"]} but got {restart_info["Config"]["Labels"]["dojo.dojo_id"]}"
     assert restart_info["Config"]["Labels"]["dojo.module_id"] == start_info["Config"]["Labels"]["dojo.module_id"], f"Expected dojo to be {start_info["Config"]["Labels"]["dojo.module_id"]} but got {restart_info["Config"]["Labels"]["dojo.module_id"]}"
     assert restart_info["Config"]["Labels"]["dojo.challenge_id"] == start_info["Config"]["Labels"]["dojo.challenge_id"], f"Expected dojo to be {start_info["Config"]["Labels"]["dojo.challenge_id"]} but got {restart_info["Config"]["Labels"]["dojo.challenge_id"]}"
 
-def test_int_restart(random_user_name):
-    starting_test(random_user_name, False)
-    restarting_test(random_user_name, False) # norm -> norm
-    restarting_test(random_user_name, True)  # norm -> priv
-    restarting_test(random_user_name, False) # priv -> priv
-    restarting_test(random_user_name, True)  # priv -> norm
+@container_running
+def test_int_restart(random_user):
+    name, _ = random_user
+    restarting_test(name, False) # norm -> norm
+    restarting_test(name, True)  # norm -> priv
+    restarting_test(name, False) # priv -> priv
+    restarting_test(name, True)  # priv -> norm
 
-def test_int_list(random_user_name):
-    token = container_token(random_user_name)
+@container_running
+def test_int_list(random_user):
+    name, _ = random_user
+    token = container_token(name)
     headers = {"type": "container", "token": token}
     result = requests.get(f"{DOJO_API}/integration/list", headers=headers)
     assert result.status_code == 200, "Failed to get list of dojos"
@@ -144,8 +165,10 @@ def test_int_list(random_user_name):
     for category, dojos in result_dojos.items():
         pass
 
-def test_int_list_priv(random_user_name):
+@container_running
+def test_int_list_priv(random_user):
     pass
 
-def test_int_info(random_user_name):
+@container_running
+def test_int_info(random_user):
     pass
