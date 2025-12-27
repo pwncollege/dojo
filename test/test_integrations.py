fc9085b9b0b40238993f32e1ac77f370b293c9fe
COM8AT <tkitzenm@asu.edu>
2025-12-27 15:29:50 -0700

    Test Dojo Restart
    Test what the result of workspace_run(dojo restart) is.
    
    Added util function to inspect user's challenge container.

--- test/test_integrations.py
+++ test/test_integrations.py
@@ -1,11 +1,19 @@
+import json
 import subprocess
 
-from utils import workspace_run, start_challenge
+from utils import dojo_run, get_outer_container_for, get_user_id, workspace_run, start_challenge
 
 CLI_INCORRECT_USAGE = 1
 CLI_TOKEN_NOT_FOUND = 2
 CLI_API_ERROR = 3
 CLI_INCORRECT = 4
+
+def inspect_container(username):
+    container_name = f"user_{get_user_id(username)}"
+    outer_container = get_outer_container_for(container_name)
+    args = [ "docker", "inspect", container_name ]
+    result = dojo_run(*args, stdin=subprocess.DEVNULL, check=True, container=outer_container).stdout
+    return json.loads(result)
 
 def test_whoami(random_user, welcome_dojo):
     """
@@ -50,7 +58,7 @@ def test_solve_correct(random_user, welcome_dojo):
         result = workspace_run("dojo submit $(< /flag)", user=name)
         assert "already been solved" in result.stdout, f"Expected to solve challenge, got: {(result.stdout, result.stderr)}"
     except subprocess.CalledProcessError as error:
-        assert False, f"Exxception when running command \"dojo submit\": {(error.stdout, error.stderr)}"
+        assert False, f"Exception when running command \"dojo submit\": {(error.stdout, error.stderr)}"
 
 def test_solve_incorrect(random_user, welcome_dojo):
     """
@@ -87,3 +95,20 @@ def test_solve_practice(random_user, welcome_dojo):
     except subprocess.CalledProcessError as error:
         assert error.returncode == CLI_INCORRECT, f"Exception when running command \"dojo submit\": {(error.stdout, error.stderr)}"
         assert "This is the practice flag" in error.stdout, f"Expected flag to be the practice flag, got: {(error.stdout, error.stderr)}"
+
+def test_restart(random_user, welcome_dojo):
+    """
+    Tests the dojo application with the "restart" command.
+
+    This test case starts a challenge, then restarts in
+    both privileged and normal modes.
+    """
+    name, session = random_user
+    start_challenge(welcome_dojo, "welcome", "flag", session=session)
+
+    try:
+        result = workspace_run("dojo restart", user=name)
+        assert False, f"Result of running command \"dojo restart\": {(result.stdout, result.stderr)}"
+    except subprocess.CalledProcessError as error:
+        assert False, f"Exception when running command \"dojo restart\": {(error.stdout, error.stderr)}"
+
