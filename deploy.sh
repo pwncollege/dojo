1fc46341b24a111c19cb70dfe20ccad7e3b8eb89
Connor Nelson <Connor@ConnorNelson.com>
2026-02-05 23:02:36 +0000

    infra: restart image-pull-worker during multinode setup

--- deploy.sh
+++ deploy.sh
@@ -306,7 +306,7 @@ if [ "$START" == "yes" -a "$MULTINODE" == "yes" ]; then
 	docker exec "$DOJO_CONTAINER" dojo-node add 1 "$NODE1_KEY"
 	docker exec "$DOJO_CONTAINER" dojo-node add 2 "$NODE2_KEY"
 	sleep 5
-	docker exec "$DOJO_CONTAINER" dojo compose restart ctfd sshd stats-worker
+	docker exec "$DOJO_CONTAINER" dojo compose restart ctfd sshd stats-worker image-pull-worker
 	sleep 5
 	docker exec "$DOJO_CONTAINER" dojo compose restart nginx
 	sleep 5
