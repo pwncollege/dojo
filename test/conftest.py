4bfbf6549151cd14c0b2d5abc29287ffbfc75aaf
COM8AT <tkitzenm@asu.edu>
2026-02-10 13:30:33 -0700

    Split id

--- test/conftest.py
+++ test/conftest.py
@@ -152,13 +152,15 @@ def lfs_dojo(admin_session):
 @pytest.fixture(scope="session")
 def event_dojo(admin_session):
     rid = create_dojo_yml(open(TEST_DOJOS_LOCATION / "event_dojo.yml").read(), session=admin_session)
+    db_id = rid.split("~")[0]
     print(rid)
-    data_raw = db_sql(f"SELECT data FROM dojos WHERE id='{rid}';")
+    print(db_id)
+    data_raw = db_sql(f"SELECT data FROM dojos WHERE id='{db_id}';")
     print(data_raw)
     data = json.loads(data_raw)
     print(data)
     data["permissions"] = ["grant_awards"]
-    db_sql(f"UPDATE dojos SET data='{json.dumps(data)}' WHERE id='{rid}';")
+    db_sql(f"UPDATE dojos SET data='{json.dumps(data)}' WHERE id='{db_id}';")
     return rid
 
 @pytest.fixture(scope="session")
