c84ac438306c4011566c11268ecf82d5e80811ca
COM8AT <tkitzenm@asu.edu>
2026-03-16 22:31:29 -0700

    Commit changes

--- migrate_emojis.py
+++ migrate_emojis.py
@@ -3,10 +3,11 @@ from CTFd.models import db
 
 def migrate():
     for award in Emojis.query.all():
-        if len(award.name) == 1:
+        if len(award.name) == 1: # Emojis with an emoji as the name, change to be CUSTOM and set icon.
             award.icon = award.name
             award.name = "CUSTOM"
-            if ":CUSTOM_AWARD:" in award.description:
+            if ":CUSTOM_AWARD:" in award.description: # Set category (origin) for custom awards that were granted.
                 dojo_id, description = award.description.split(":CUSTOM_AWARD:")
                 award.description = description
                 award.category = dojo_id
+    db.session.commit()
