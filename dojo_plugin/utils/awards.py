99644851088a9ed9ede243ca4239e527fee54900
COM8AT <tkitzenm@asu.edu>
2026-04-14 11:07:36 -0700

    Revert to "None" icon

--- dojo_plugin/utils/awards.py
+++ dojo_plugin/utils/awards.py
@@ -125,7 +125,7 @@ def update_awards(user):
             
         display_name = dojo.name or dojo.reference_id
         description = f"Awarded for completing the {display_name} dojo."
-        db.session.add(Emojis(user=user, name="CURRENT", description=description, category=hex_dojo_id, icon=""))
+        db.session.add(Emojis(user=user, name="CURRENT", description=description, category=hex_dojo_id, icon=None))
         db.session.commit()
         
         if dojo.official or dojo.data.get("type") == "public":
