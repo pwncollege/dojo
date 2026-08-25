7179cdc7b0662a2c3cdf7e9421501381dd0308d1
Connor Nelson <Connor@ConnorNelson.com>
2026-08-25 01:46:01 +0000

    Workspace: simplify terminal service derivation

--- workspace/services/terminal.nix
+++ workspace/services/terminal.nix
@@ -3,18 +3,16 @@
 let
   service = import ./service.nix { inherit pkgs; };
 
-  libwebsockets' = pkgs.libwebsockets.overrideAttrs (previousAttrs: {
-    cmakeFlags = previousAttrs.cmakeFlags ++ [
-      (pkgs.lib.cmakeBool "LWS_WITH_SOCKS5" false)
-      (pkgs.lib.cmakeBool "LWS_WITHOUT_CLIENT" true)
-    ];
-  });
+  ttyd = pkgs.ttyd.override {
+    libwebsockets = pkgs.libwebsockets.overrideAttrs (previousAttrs: {
+      cmakeFlags = previousAttrs.cmakeFlags ++ [
+        (pkgs.lib.cmakeBool "LWS_WITH_SOCKS5" false)
+        (pkgs.lib.cmakeBool "LWS_WITHOUT_CLIENT" true)
+      ];
+    });
+  };
 
-  ttyd = pkgs.ttyd.override { libwebsockets = libwebsockets'; };
-
-  serviceScript = pkgs.writeScript "dojo-terminal" ''
-    #!${pkgs.bash}/bin/bash
-
+  serviceScript = pkgs.writeShellScript "dojo-terminal" ''
     until [ -f /run/dojo/var/ready ]; do sleep 0.1; done
 
     export TERM=xterm-256color
@@ -30,24 +28,9 @@ let
     until ${pkgs.curl}/bin/curl -fs localhost:7681 >/dev/null; do sleep 0.1; done
   '';
 
-in pkgs.stdenv.mkDerivation {
-  name = "terminal-service";
-  buildInputs = [
-    ttyd
-    pkgs.bashInteractive
-    pkgs.curl
-  ];
-  dontUnpack = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin
-    cp ${serviceScript} $out/bin/dojo-terminal
-    chmod +x $out/bin/dojo-terminal
-    ln -s ${ttyd}/bin/ttyd $out/bin/ttyd
-    ln -s ${ttyd}/bin/ttyd $out/bin/terminal
-
-    runHook postInstall
-  '';
-}
+in
+pkgs.runCommand "terminal-service" { } ''
+  install -Dm755 ${serviceScript} $out/bin/dojo-terminal
+  ln -s ${ttyd}/bin/ttyd $out/bin/ttyd
+  ln -s ttyd $out/bin/terminal
+''
